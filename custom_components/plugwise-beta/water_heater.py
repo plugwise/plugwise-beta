da9600601d62e6e7b7bfe64fa24ce1280e09c634
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-03-26 19:37:24 +0100

    Change water_heater states to 'on' and 'off', clean up

--- custom_components/plugwise-beta/water_heater.py
+++ custom_components/plugwise-beta/water_heater.py
@@ -15,30 +15,12 @@ from homeassistant.core import callback
 
 from homeassistant.helpers.entity import Entity
 
-from homeassistant.components.climate.const import (
-    CURRENT_HVAC_HEAT,
-    CURRENT_HVAC_COOL,
-    CURRENT_HVAC_IDLE,
-    HVAC_MODE_AUTO,
-    HVAC_MODE_HEAT,
-    HVAC_MODE_HEAT_COOL,
-    HVAC_MODE_OFF,
-    SUPPORT_PRESET_MODE,
-    SUPPORT_TARGET_TEMPERATURE,
-)
-
 from homeassistant.const import (
-    CONF_HOST,
-    CONF_NAME,
-    CONF_PASSWORD,
-    CONF_PORT,
-    CONF_USERNAME,
-    TEMP_CELSIUS,
+    STATE_OFF,
+    STATE_ON,
 )
 
 from .const import (
-    CONF_THERMOSTAT,
-    CURRENT_HVAC_DHW,
     DOMAIN,
     WATER_HEATER_ICON,
 )
@@ -48,35 +30,10 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-# Scan interval for updating sensor values
-# Smile communication is set using configuration directives
-SCAN_INTERVAL = timedelta(seconds=60)
-
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Smile sensors from a config entry."""
     api = hass.data[DOMAIN][config_entry.entry_id]['api']
     updater = hass.data[DOMAIN][config_entry.entry_id]['updater']
-
-#    # Stay close to meter measurements for power, for thermostat back off a bit
-#    if api._smile_type == 'power':
-#        update_interval=timedelta(seconds=10)
-#    else:
-#        update_interval=timedelta(seconds=60)
-#
-#    wh_coordinator = DataUpdateCoordinator(
-#        hass,
-#        _LOGGER,
-#        name="water_heater",
-#        update_method=partial(async_safe_fetch,api),
-#        update_interval=update_interval
-#    )
-#
-#    # First do a refresh to see if we can reach the hub.
-#    # Otherwise we will declare not ready.
-#    await wh_coordinator.async_refresh()
-#
-#    if not wh_coordinator.last_update_success:
-#        raise PlatformNotReady
 
     devices = []
     all_devices=api.get_all_devices()
@@ -96,24 +53,16 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     async_add_entities(devices, True)
 
-#async def async_safe_fetch(api):
-#    """Safely fetch data."""
-#    with async_timeout.timeout(10):
-#        await api.full_update_device()
-#        return await api.get_devices()
-#
+
 class PwWaterHeater(Entity):
     """Representation of a Plugwise water_heater."""
 
-    # def __init__(self, coordinator, idx, api, name, dev_id, ctlr_id):
     def __init__(self, api, updater, name, dev_id):
         """Set up the Plugwise API."""
         self._api = api
         self._updater = updater
         self._name = name
         self._dev_id = dev_id
-        #self._heating_status =  None
-        #self._boiler_status = None
         self._domestic_hot_water_state = None
         self._unique_id = f"{dev_id}-water_heater"
 
@@ -154,11 +103,9 @@ class PwWaterHeater(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        #if self._heating_status or self._boiler_status:
-        #    return CURRENT_HVAC_HEAT
         if self._domestic_hot_water_state:
-            return CURRENT_HVAC_DHW
-        return CURRENT_HVAC_IDLE
+            return STATE_ON
+        return STATE_OFF
 
     @property
     def icon(self):
@@ -179,9 +126,5 @@ class PwWaterHeater(Entity):
         if data is None:
             _LOGGER.debug("Received no data for device %s.", self._name)
         else:
-            #if 'central_heating_state' in data:
-            #    self._heating_status =  data['central_heating_state']
-            #if 'boiler_state' in data:
-            #    self._boiler_status = data['boiler_state']
             if 'domestic_hot_water_state' in data:
                 self._domestic_hot_water_state = (data['domestic_hot_water_state'] == 'on')
