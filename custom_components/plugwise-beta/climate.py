9cbc93d3b04b093594fcf4fec4d181a8ce9b7bb6
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-04-26 20:07:31 +0200

    Add try-except when using await

--- custom_components/plugwise-beta/climate.py
+++ custom_components/plugwise-beta/climate.py
@@ -241,9 +241,12 @@ class PwThermostat(ClimateDevice):
             self._min_temp < temperature < self._max_temp
         ):
             _LOGGER.debug("Set temp to %sºC", temperature)
-            await self._api.set_temperature(self._loc_id, temperature)
-            self._setpoint = temperature
-            self.async_write_ha_state()
+            try:
+                await self._api.set_temperature(self._loc_id, temperature)
+                self._setpoint = temperature
+                self.async_write_ha_state()
+            except Smile.PlugwiseError:
+                _LOGGER.error("Error while communicating to device")
         else:
             _LOGGER.error("Invalid temperature requested")
 
@@ -253,21 +256,30 @@ class PwThermostat(ClimateDevice):
         state = "false"
         if hvac_mode == HVAC_MODE_AUTO:
             state = "true"
-            await self._api.set_temperature(self._loc_id, self._schedule_temp)
-            self._setpoint = self._schedule_temp
-        await self._api.set_schedule_state(
-            self._loc_id, self._last_active_schema, state
-        )
-        self._hvac_mode = hvac_mode
-        self.async_write_ha_state()
+            try:
+                await self._api.set_temperature(self._loc_id, self._schedule_temp)
+                self._setpoint = self._schedule_temp
+            except Smile.PlugwiseError:
+                _LOGGER.error("Error while communicating to device")
+        try:
+            await self._api.set_schedule_state(
+                self._loc_id, self._last_active_schema, state
+            )
+            self._hvac_mode = hvac_mode
+            self.async_write_ha_state()
+        except Smile.PlugwiseError:
+            _LOGGER.error("Error while communicating to device")
 
     async def async_set_preset_mode(self, preset_mode):
         """Set the preset mode."""
         _LOGGER.debug("Set preset mode to %s.", preset_mode)
-        await self._api.set_preset(self._loc_id, preset_mode)
-        self._preset_mode = preset_mode
-        self._setpoint = self._presets.get(self._preset_mode, "none")[0]
-        self.async_write_ha_state()
+        try:
+            await self._api.set_preset(self._loc_id, preset_mode)
+            self._preset_mode = preset_mode
+            self._setpoint = self._presets.get(self._preset_mode, "none")[0]
+            self.async_write_ha_state()
+        except Smile.PlugwiseError:
+            _LOGGER.error("Error while communicating to device")
 
     def update(self):
         """Update the data for this climate device."""
