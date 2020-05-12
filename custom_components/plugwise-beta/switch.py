a9a471bf1531a4bd064ca1621bfd21a78ac36845
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-05-12 19:33:04 +0200

    Update switch.py

--- custom_components/plugwise-beta/switch.py
+++ custom_components/plugwise-beta/switch.py
@@ -4,10 +4,10 @@ import logging
 from typing import Dict
 
 from homeassistant.components.switch import SwitchDevice
-from homeassistant.core import callback
-from Plugwise_Smile.Smile import Smile
 
 from .const import DOMAIN, SWITCH_ICON
+
+from . import SmileGateway
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,7 +15,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Smile switches from a config entry."""
     api = hass.data[DOMAIN][config_entry.entry_id]["api"]
-    updater = hass.data[DOMAIN][config_entry.entry_id]["updater"]
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
 
     devices = []
     all_devices = api.get_all_devices()
@@ -23,19 +23,20 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         if "plug" in device["types"]:
             model = "Metered Switch"
             _LOGGER.debug("Plugwise switch Dev %s", device["name"])
-            devices.append(PwSwitch(api, updater, device["name"], dev_id, model,))
+            devices.append(PwSwitch(api, coordinator, device["name"], dev_id, model,))
             _LOGGER.info("Added switch.%s", "{}".format(device["name"]))
 
     async_add_entities(devices, True)
 
 
-class PwSwitch(SwitchDevice):
+class PwSwitch(SmileGateway, SwitchDevice):
     """Representation of a Plugwise plug."""
 
-    def __init__(self, api, updater, name, dev_id, model):
+    def __init__(self, api, coordinator, name, dev_id, model):
         """Set up the Plugwise API."""
+        super().__init__(api, coordinator)
+
         self._api = api
-        self._updater = updater
         self._model = model
         self._name = name
         self._dev_id = dev_id
@@ -46,20 +47,6 @@ class PwSwitch(SwitchDevice):
     def unique_id(self):
         """Return a unique ID."""
         return self._unique_id
-
-    async def async_added_to_hass(self):
-        """Register callbacks."""
-        self._updater.async_add_listener(self._update_callback)
-
-    async def async_will_remove_from_hass(self):
-        """Disconnect callbacks."""
-        self._updater.async_remove_listener(self._update_callback)
-
-    @callback
-    def _update_callback(self):
-        """Call update method."""
-        self.update()
-        self.async_write_ha_state()
 
     @property
     def is_on(self):
@@ -80,11 +67,6 @@ class PwSwitch(SwitchDevice):
             "model": self._model,
             "via_device": via_device,
         }
-
-    @property
-    def should_poll(self):
-        """No need to poll. Coordinator notifies entity of updates."""
-        return False
 
     async def turn_on(self, **kwargs):
         """Turn the device on."""
