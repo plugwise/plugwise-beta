86f8151f9c4f0ec4373459bf625a36dff045a1ea
Tom Scholten <tom@sue.nl>
2020-05-26 09:57:36 +0200

    Initial review for 111

--- custom_components/plugwise-beta/config_flow.py
+++ custom_components/plugwise-beta/config_flow.py
@@ -51,6 +51,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             try:
                 api = await validate_input(self.hass, user_input)
 
+                await self.async_set_unique_id(api.gateway_id)
+                self._abort_if_unique_id_configured()
+
                 return self.async_create_entry(api.smile_name, data=user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
@@ -59,9 +62,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
-
-            await self.async_set_unique_id(api.gateway_id)
-            self._abort_if_unique_id_configured()
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
