2d4db5de6a236abc76b7bb7cdb094322d1354ffc
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-05-31 11:25:56 +0200

    Consts for typemapping - from HA PR

--- custom_components/plugwise-beta/const.py
+++ custom_components/plugwise-beta/const.py
@@ -2,6 +2,11 @@
 
 DOMAIN = "plugwise-beta"
 
+# Sensor mapping
+SENSOR_MAP_MODEL = 0
+SENSOR_MAP_UOM = 1
+SENSOR_MAP_DEVICE_CLASS = 2
+SENSOR_MAP_ICON = 3
 
 # Default directives
 DEFAULT_NAME = "Smile"
