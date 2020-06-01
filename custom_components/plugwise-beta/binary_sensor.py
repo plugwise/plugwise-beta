535b0a78b91cf06874863e3c9d92a72f7e7e045a
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-06-01 11:46:32 +0200

    Update binary_sensor

--- custom_components/plugwise-beta/binary_sensor.py
+++ custom_components/plugwise-beta/binary_sensor.py
@@ -2,18 +2,17 @@
 
 import logging
 
-from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.binary_sensor import BinarySensorEntity, DEVICE_CLASS_OPENING
 from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import callback
 
-from .const import DOMAIN, FLAME_ICON, VALVE_CLOSED_ICON, VALVE_OPEN_ICON, WATER_ICON
+from .const import DOMAIN, FLOW_OFF_ICON, FLOW_ON_ICON, IDLE_ICON, FLAME_ICON
 from .sensor import SmileSensor
 
-BINARY_SENSOR_LIST = [
-    "dhw_state",
-    "slave_boiler_state",
-    "valve_position",
-]
+BINARY_SENSOR_MAP = {
+    "dhw_state": ["Domestic Hot Water State", None],
+    "slave_boiler_state": ["Secondary Heater Device State", None],
+}
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,18 +23,13 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
 
     entities = []
-    binary_sensor_classes = [
-        "heater_central",
-        "thermo_sensor",
-        "thermostatic_radiator_valve",
-    ]
 
     all_devices = api.get_all_devices()
     for dev_id, device_properties in all_devices.items():
-        if device_properties["class"] in binary_sensor_classes:
+        if device_properties["class"] == "heater_central":
             _LOGGER.debug("Plugwise device_class %s found", device_properties["class"])
             data = api.get_device_data(dev_id)
-            for binary_sensor in BINARY_SENSOR_LIST:
+            for binary_sensor, b_s_type in BINARY_SENSOR_MAP.items():
                 _LOGGER.debug("Binary_sensor: %s", binary_sensor)
                 if binary_sensor in data:
                     _LOGGER.debug(
@@ -92,22 +86,18 @@ class PwBinarySensor(SmileSensor, BinarySensorEntity):
             return
 
         if self._binary_sensor in data:
-
-            if isinstance(data[self._binary_sensor], float):
-                self._is_on = data[self._binary_sensor] == 1.0
             self._is_on = data[self._binary_sensor]
 
             self._state = STATE_OFF
+            if self._binary_sensor == "dhw_state":
+                self._icon = FLOW_OFF_ICON
+            if self._binary_sensor == "slave_boiler_state":
+                self._icon = IDLE_ICON
             if self._is_on:
                 self._state = STATE_ON
-
-            if self._binary_sensor == "dhw_state":
-                self._icon = WATER_ICON
-            if self._binary_sensor == "slave_boiler_state":
-                self._icon = FLAME_ICON
-            if self._binary_sensor == "valve_position":
-                self._icon = VALVE_CLOSED_ICON
-                if self._is_on:
-                    self._icon = VALVE_OPEN_ICON
+                if self._binary_sensor == "dhw_state":
+                    self._icon = FLOW_ON_ICON
+                if self._binary_sensor == "slave_boiler_state":
+                    self._icon = FLAME_ICON
 
         self.async_write_ha_state()
