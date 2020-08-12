a4e3617d14cc9f69d9c2d020151e0d6e5e4c13d4
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-08-12 13:10:28 +0200

    Update config_flow.py

--- custom_components/plugwise-beta/config_flow.py
+++ custom_components/plugwise-beta/config_flow.py
@@ -21,9 +21,9 @@ from .const import DEFAULT_SCAN_INTERVAL, DOMAIN  # pylint:disable=unused-import
 _LOGGER = logging.getLogger(__name__)
 
 ZEROCONF_MAP = {
-    "smile": "P1 DSMR",
-    "smile_thermo": "Climate (Anna)",
-    "smile_open_therm": "Climate (Adam)",
+    "smile": "P1",
+    "smile_thermo": "Anna",
+    "smile_open_therm": "Adam",
 }
 
 
@@ -42,7 +42,6 @@ def _base_schema(discovery_info):
 async def validate_input(hass: core.HomeAssistant, data):
     """
     Validate the user input allows us to connect.
-
     Data has the keys from _base_schema() with values provided by the user.
     """
     websession = async_get_clientsession(hass, verify_ssl=False)
@@ -87,7 +86,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         _LOGGER.debug("Discovery info: %s", self.discovery_info)
         _properties = self.discovery_info.get("properties")
 
-        await self.async_set_unique_id(api.hostname)
+        unique_id = self.discovery_info.get("hostname").split(".")[0]
+        await self.async_set_unique_id(unique_id)
         self._abort_if_unique_id_configured()
 
         _product = _properties.get("product", None)
@@ -109,11 +109,21 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
 
+            for entry in self._async_current_entries():
+                if entry.data.get(CONF_HOST) == user_input[CONF_HOST]:
+                    return self.async_abort(reason="already_configured")
+
             if self.discovery_info:
                 user_input[CONF_HOST] = self.discovery_info[CONF_HOST]
 
             try:
                 api = await validate_input(self.hass, user_input)
+                unique_id = api.gateway_id
+                smile_hostname = api.smile_hostname
+                if smile_hostname is not None:
+                    unique_id = smile_hostname
+                await self.async_set_unique_id(unique_id, raise_on_progress=False)
+                self._abort_if_unique_id_configured()
 
                 return self.async_create_entry(title=api.smile_name, data=user_input)
             except CannotConnect:
@@ -123,15 +133,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors[CONF_BASE] = "unknown"
-
-            if not errors:
-                unique_id = api.gateway_id
-                if api.hostname is not None:
-                    unique_id =  api.hostname
-                await self.async_set_unique_id(unique_id)
-                self._abort_if_unique_id_configured()
-
-                return self.async_create_entry(title=api.smile_name, data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=_base_schema(self.discovery_info), errors=errors or {}
