6aa7cee0b2150ab16479bea4cf39e6e063312ed7
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-09-07 20:22:38 +0200

    Use COORDINATOR

--- custom_components/plugwise/binary_sensor.py
+++ custom_components/plugwise/binary_sensor.py
@@ -8,6 +8,7 @@ from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.core import callback
 
 from .const import (
+    COORDINATOR,
     DOMAIN,
     FLAME_ICON,
     FLOW_OFF_ICON,
@@ -30,7 +31,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Smile binary_sensors from a config entry."""
     api = hass.data[DOMAIN][config_entry.entry_id]["api"]
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+    coordinator = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
 
     entities = []
 
