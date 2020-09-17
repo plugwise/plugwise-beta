ef706c092f9521d2191c73b778c3177e8ae86d58
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-09-17 20:49:29 +0200

    Config_flow fixes 2

--- custom_components/plugwise/config_flow.py
+++ custom_components/plugwise/config_flow.py
@@ -104,6 +104,7 @@ class PlugwiseConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
         self.context["title_placeholders"] = {
             CONF_HOST: self.discovery_info[CONF_HOST],
+            CONF_PORT: self.discovery_info[CONF_PORT],
             CONF_NAME: _name,
         }
         return await self.async_step_user()
@@ -116,6 +117,7 @@ class PlugwiseConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             if self.discovery_info:
                 user_input[CONF_HOST] = self.discovery_info[CONF_HOST]
+                user_input[CONF_PORT] = self.discovery_info[CONF_PORT]
 
             for entry in self._async_current_entries():
                 if entry.data.get(CONF_HOST) == user_input[CONF_HOST]:
