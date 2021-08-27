ce26484add145b97a25bda01b94f8a29398b5d08
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-08-27 18:02:54 +0200

    Update binary_sensor

--- custom_components/plugwise/binary_sensor.py
+++ custom_components/plugwise/binary_sensor.py
@@ -117,18 +117,18 @@ async def async_setup_entry_gateway(hass, config_entry, async_add_entities):
     coordinator = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
 
     entities = []
-    for dev_id in coordinator.data:
-        for key in coordinator.data[dev_id]:
+    for dev_id in coordinator.data[1]:
+        for key in coordinator.data[1][dev_id]:
             if key != "binary_sensors":
                 continue
 
-            for data in coordinator.data[dev_id]["binary_sensors"]:
+            for data in coordinator.data[1][dev_id]["binary_sensors"]:
                 entities.append(
                     GwBinarySensor(
                         api,
                         coordinator,
                         dev_id,
-                        coordinator.data[dev_id].get(ATTR_NAME),
+                        coordinator.data[1][dev_id].get(ATTR_NAME),
                         data,
                     )
                 )
@@ -153,21 +153,20 @@ class GwBinarySensor(SmileGateway, BinarySensorEntity):
             coordinator,
             dev_id,
             name,
-            coordinator.data[dev_id].get(PW_MODEL),
-            coordinator.data[dev_id].get(VENDOR),
-            coordinator.data[dev_id].get(FW),
+            coordinator.data[1][dev_id].get(PW_MODEL),
+            coordinator.data[1][dev_id].get(VENDOR),
+            coordinator.data[1][dev_id].get(FW),
         )
 
-        self._gw_b_sensor = GWBinarySensor(api, dev_id, bs_data[ATTR_ID])
-
         self._cdata = coordinator.data
+        self._gw_b_sensor = GWBinarySensor(self._cdata, dev_id, bs_data[ATTR_ID])
         self._attributes = {}
         self._binary_sensor = bs_data.get(ATTR_ID)
         self._bs_data = bs_data
         self._dev_id = dev_id
         self._device_class = None
         self._device_name = name
-        if self._cdata[self._dev_id][PW_CLASS] == "gateway":
+        if self._cdata[1][self._dev_id][PW_CLASS] == "gateway":
             self._device_name = f"Smile {name}"
         self._enabled_default = self._bs_data.get(ATTR_ENABLED_DEFAULT)
         self._icon = None
