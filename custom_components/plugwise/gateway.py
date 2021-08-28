450199542abaea36e6ae89aa4af1fea602ee7818
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-08-28 19:53:54 +0200

    Create coordinator.py, update gateway.py accordingly

--- custom_components/plugwise/gateway.py
+++ custom_components/plugwise/gateway.py
@@ -47,6 +47,7 @@ from .const import (
     SERVICE_DELETE,
     UNDO_UPDATE_LISTENER,
 )
+from .coordinator import PWDataUpdateCoordinator
 
 CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
@@ -174,37 +175,6 @@ async def _update_listener(hass: HomeAssistant, entry: ConfigEntry):
     coordinator.update_interval = timedelta(
         seconds=entry.options.get(CONF_SCAN_INTERVAL)
     )
-
-
-class PWDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching Plugwise API data from a single endpoint."""
-    def __init__(self, hass, api, update_interval):
-        """Initialize the coordinator."""
-        super().__init__(
-            hass,
-            _LOGGER, 
-            name=f"{api.smile_name}",
-            update_interval = update_interval
-        )
-        self._api = api
-        self._u_interval = update_interval
-
-    async def _async_update_data(self):
-        """Update data via API endpoint."""
-        _LOGGER.debug("Updating %s", self._api.smile_name)
-        try:
-            with timeout(self._u_interval.seconds):
-                data = await self._api.update()
-                return data
-                _LOGGER.debug("Successfully updated %s", self._api.smile_name)
-        except XMLDataMissingError as err:
-            _LOGGER.debug(
-                "Updating Smile failed, expected XML data for %s", self._api.smile_name
-            )
-            raise UpdateFailed("Smile update failed") from err
-        except PlugwiseException as err:
-            _LOGGER.debug("Updating failed, generic failure for %s", self._api.smile_name)
-            raise UpdateFailed("Smile update failed") from err
 
 
 class SmileGateway(CoordinatorEntity):
