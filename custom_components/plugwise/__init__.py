953a0ed25d861277d2daf903f8a8855ab0970453
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-11-17 17:40:17 +0100

    Clean __init__.py

--- custom_components/plugwise/__init__.py
+++ custom_components/plugwise/__init__.py
@@ -4,30 +4,20 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 
-from .const import CONF_USB_PATH
-
 from .gateway import async_setup_entry_gw, async_unload_entry_gw
-from .usb import async_setup_entry_usb, async_unload_entry_usb
-
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Plugwise platform."""
-    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Plugwise components from a config entry."""
     if entry.data.get(CONF_HOST):
         return await async_setup_entry_gw(hass, entry)
-    if entry.data.get(CONF_USB_PATH):
-        return await async_setup_entry_usb(hass, entry)
+    # PLACEHOLDER USB entry setup
     return False
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload the Plugwise components."""
     if entry.data.get(CONF_HOST):
         return await async_unload_entry_gw(hass, entry)
-    if entry.data.get(CONF_USB_PATH):
-        return await async_unload_entry_usb(hass, entry)
+    # PLACEHOLDER USB entry setup
     return False
