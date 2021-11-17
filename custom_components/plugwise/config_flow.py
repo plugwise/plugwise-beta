caeb83ec028e49b02a8a4a257405139764b2bd07
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-11-17 19:29:13 +0100

    Clean config_flow.py

--- custom_components/plugwise/config_flow.py
+++ custom_components/plugwise/config_flow.py
@@ -4,10 +4,7 @@ from __future__ import annotations
 
 import logging
 
-import serial.tools.list_ports
 import voluptuous as vol
-from homeassistant import config_entries, core, exceptions
-from homeassistant.components import usb
 from homeassistant.const import (
     CONF_BASE,
     CONF_HOST,
@@ -23,18 +20,12 @@ from homeassistant.helpers.typing import DiscoveryInfoType
 
 from plugwise.exceptions import (
     InvalidAuthentication,
-    NetworkDown,
     PlugwiseException,
-    PortError,
-    StickInitError,
-    TimeoutException,
 )
 from plugwise.smile import Smile
-from plugwise.stick import Stick
 
 from .const import (
     API,
-    CONF_USB_PATH,
     DEFAULT_PORT,
     DEFAULT_SCAN_INTERVAL,
     DEFAULT_USERNAME,
@@ -46,7 +37,6 @@ from .const import (
     FLOW_USB,
     PW_TYPE,
     SMILE,
-    STICK,
     STRETCH,
     STRETCH_USERNAME,
     ZEROCONF_MAP,
@@ -61,40 +51,7 @@ CONNECTION_SCHEMA = vol.Schema(
 )
 
 
-@callback
-def plugwise_stick_entries(hass):
-    """Return existing connections for Plugwise USB-stick domain."""
-    sticks = []
-    for entry in hass.config_entries.async_entries(DOMAIN):
-        if entry.data.get(PW_TYPE) == STICK:
-            sticks.append(entry.data.get(CONF_USB_PATH))
-    return sticks
-
-
-async def validate_usb_connection(self, device_path=None) -> dict[str, str]:
-    """Test if device_path is a real Plugwise USB-Stick."""
-    errors = {}
-
-    # Avoid creating a 2nd connection to an already configured stick
-    if device_path in plugwise_stick_entries(self):
-        errors[CONF_BASE] = "already_configured"
-        return errors, None
-
-    api_stick = await self.async_add_executor_job(Stick, device_path)
-    try:
-        await self.async_add_executor_job(api_stick.connect)
-        await self.async_add_executor_job(api_stick.initialize_stick)
-        await self.async_add_executor_job(api_stick.disconnect)
-    except PortError:
-        errors[CONF_BASE] = "cannot_connect"
-    except StickInitError:
-        errors[CONF_BASE] = "stick_init"
-    except NetworkDown:
-        errors[CONF_BASE] = "network_down"
-    except TimeoutException:
-        errors[CONF_BASE] = "network_timeout"
-    return errors, api_stick
-
+# PLACEHOLDER USB connection validation
 
 def _base_gw_schema(discovery_info):
     """Generate base schema for gateways."""
@@ -177,67 +134,7 @@ class PlugwiseConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         }
         return await self.async_step_user_gateway()
 
-    async def async_step_user_usb(self, user_input=None):
-        """Step when user initializes a integration."""
-        errors = {}
-        ports = await self.hass.async_add_executor_job(serial.tools.list_ports.comports)
-        list_of_ports = [
-            f"{p}, s/n: {p.serial_number or 'n/a'}"
-            + (f" - {p.manufacturer}" if p.manufacturer else "")
-            for p in ports
-        ]
-        list_of_ports.append(CONF_MANUAL_PATH)
-
-        if user_input is not None:
-            user_input.pop(FLOW_TYPE, None)
-            user_selection = user_input[CONF_USB_PATH]
-
-            if user_selection == CONF_MANUAL_PATH:
-                return await self.async_step_manual_path()
-
-            port = ports[list_of_ports.index(user_selection)]
-            device_path = await self.hass.async_add_executor_job(
-                usb.get_serial_by_id, port.device
-            )
-            errors, api_stick = await validate_usb_connection(self.hass, device_path)
-            if not errors:
-                await self.async_set_unique_id(api_stick.mac)
-                return self.async_create_entry(
-                    title="Stick", data={CONF_USB_PATH: device_path, PW_TYPE: STICK}
-                )
-        return self.async_show_form(
-            step_id="user_usb",
-            data_schema=vol.Schema(
-                {vol.Required(CONF_USB_PATH): vol.In(list_of_ports)}
-            ),
-            errors=errors,
-        )
-
-    async def async_step_manual_path(self, user_input=None):
-        """Step when manual path to device."""
-        errors = {}
-        if user_input is not None:
-            user_input.pop(FLOW_TYPE, None)
-            device_path = await self.hass.async_add_executor_job(
-                usb.get_serial_by_id, user_input.get(CONF_USB_PATH)
-            )
-            errors, api_stick = await validate_usb_connection(self.hass, device_path)
-            if not errors:
-                await self.async_set_unique_id(api_stick.mac)
-                return self.async_create_entry(
-                    title="Stick", data={CONF_USB_PATH: device_path}
-                )
-        return self.async_show_form(
-            step_id="manual_path",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_USB_PATH, default="/dev/ttyUSB0" or vol.UNDEFINED
-                    ): str
-                }
-            ),
-            errors=errors,
-        )
+# PLACEHOLDER USB step_user
 
     async def async_step_user_gateway(self, user_input=None):
         """Handle the initial step when using network/gateway setups."""
@@ -308,13 +205,7 @@ class PlugwiseOptionsFlowHandler(config_entries.OptionsFlow):
         """Initialize options flow."""
         self.config_entry = config_entry
 
-    async def async_step_none(self, user_input=None):
-        """No options available."""
-        if user_input is not None:
-            # Apparently not possible to abort an options flow at the moment
-            return self.async_create_entry(title="", data=self.config_entry.options)
-
-        return self.async_show_form(step_id="none")
+    # PLACEHOLDER USB async_step_none
 
     async def async_step_init(self, user_input=None):
         """Manage the Plugwise options."""
