d3f67b6c52fe0266cb629121982c481e6bbdcc1d
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-11-17 19:42:34 +0100

    Clean sensor.py

--- custom_components/plugwise/sensor.py
+++ custom_components/plugwise/sensor.py
@@ -8,24 +8,17 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import ATTR_ID, ATTR_NAME, ATTR_STATE
 from homeassistant.core import callback
 
-from plugwise.nodes import PlugwiseNode
-
 from .const import (
-    CB_NEW_NODE,
     COORDINATOR,
     DOMAIN,
     FW,
     PW_MODEL,
-    PW_TYPE,
     SMILE,
-    STICK,
-    USB,
     VENDOR,
 )
 from .gateway import SmileGateway
 from .models import PW_SENSOR_TYPES, PlugwiseSensorEntityDescription
 from .smile_helpers import icon_selector
-from .usb import PlugwiseUSBEntity
 
 PARALLEL_UPDATES = 0
 
@@ -34,40 +27,8 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Smile switches from a config entry."""
-    if hass.data[DOMAIN][config_entry.entry_id][PW_TYPE] == USB:
-        return await async_setup_entry_usb(hass, config_entry, async_add_entities)
-    # Considered default and for earlier setups without usb/network config_flow
+    # PLACEHOLDER for async_setup_entry_usb()
     return await async_setup_entry_gateway(hass, config_entry, async_add_entities)
-
-
-async def async_setup_entry_usb(hass, config_entry, async_add_entities):
-    """Set up Plugwise sensor based on config_entry."""
-    api_stick = hass.data[DOMAIN][config_entry.entry_id][STICK]
-
-    async def async_add_sensors(mac: str):
-        """Add plugwise sensors for device."""
-        entities = []
-        entities.extend(
-            [
-                USBSensor(api_stick.devices[mac], description)
-                for description in PW_SENSOR_TYPES
-                if description.plugwise_api == STICK
-                and description.key in api_stick.devices[mac].features
-            ]
-        )
-        if entities:
-            async_add_entities(entities)
-
-    for mac in hass.data[DOMAIN][config_entry.entry_id][SENSOR_DOMAIN]:
-        hass.async_create_task(async_add_sensors(mac))
-
-    def discoved_device(mac: str):
-        """Add sensors for newly discovered device."""
-        hass.async_create_task(async_add_sensors(mac))
-
-    # Listen for discovered nodes
-    api_stick.subscribe_stick_callback(discoved_device, CB_NEW_NODE)
-
 
 async def async_setup_entry_gateway(hass, config_entry, async_add_entities):
     """Set up the Smile sensors from a config entry."""
@@ -139,21 +100,3 @@ class GwSensor(SmileGateway, SensorEntity):
             self._attr_icon = icon_selector(self._attr_native_value, None)
 
         self.async_write_ha_state()
-
-
-class USBSensor(PlugwiseUSBEntity, SensorEntity):
-    """Representation of a Plugwise USB sensor."""
-
-    def __init__(
-        self, node: PlugwiseNode, description: PlugwiseSensorEntityDescription
-    ) -> None:
-        """Initialize sensor entity."""
-        super().__init__(node, description)
-
-    @property
-    def native_value(self) -> float | None:
-        """Return the native value of the sensor."""
-        state_value = getattr(self._node, self.entity_description.state_request_method)
-        if state_value is not None:
-            return float(round(state_value, 3))
-        return None
