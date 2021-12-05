7cd53d97aebf82f616bf1e31b283a45e3295b542
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-12-05 10:34:51 +0100

    Remove server-entries

--- tests/components/plugwise/test_config_flow.py
+++ tests/components/plugwise/test_config_flow.py
@@ -53,9 +53,8 @@ TEST_USERNAME2 = "stretch"
 
 TEST_DISCOVERY = zeroconf.ZeroconfServiceInfo(
     host=TEST_HOST,
-    port=DEFAULT_PORT,
     hostname=f"{TEST_HOSTNAME}.local.",
-    server=f"{TEST_HOSTNAME}.local.",
+    port=DEFAULT_PORT,
     properties={
         "product": "smile",
         "version": "1.2.3",
@@ -64,9 +63,8 @@ TEST_DISCOVERY = zeroconf.ZeroconfServiceInfo(
 )
 TEST_DISCOVERY2 = zeroconf.ZeroconfServiceInfo(
     host=TEST_HOST,
-    port=DEFAULT_PORT,
     hostname=f"{TEST_HOSTNAME2}.local.",
-    server=f"{TEST_HOSTNAME2}.local.",
+    port=DEFAULT_PORT,
     properties={
         "product": "stretch",
         "version": "1.2.3",
