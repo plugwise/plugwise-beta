a98a2da29a83cefbba7fc04b87aa8200c8a81e0f
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-02-13 14:13:37 +0100

    Add missing timedelta import

--- custom_components/plugwise/gateway.py
+++ custom_components/plugwise/gateway.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import asyncio
+from datetime import timedelta
 from typing import Any
 
 from plugwise.exceptions import InvalidAuthentication, PlugwiseException
