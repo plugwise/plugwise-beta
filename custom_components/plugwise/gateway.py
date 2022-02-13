e9e101f9de52851057cc0ab2a733ca1508e12646
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-02-13 13:11:52 +0100

    Process gateway.py

--- custom_components/plugwise/gateway.py
+++ custom_components/plugwise/gateway.py
@@ -1,69 +1,40 @@
 """Plugwise network/gateway platform."""
-
 from __future__ import annotations
 
 import asyncio
-import logging
-from datetime import timedelta
+from typing import Any
 
-import voluptuous as vol
 from plugwise.exceptions import InvalidAuthentication, PlugwiseException
 from plugwise.smile import Smile
 
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_PORT, CONF_USERNAME
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_PASSWORD,
-    CONF_PORT,
-    CONF_SCAN_INTERVAL,
-    CONF_USERNAME,
-    Platform,
-)
+from homeassistant.helpers.entity_registry import RegistryEntry, async_migrate_entries
 
 from .const import (
-    API,
-    COORDINATOR,
     DEFAULT_PORT,
-    DEFAULT_SCAN_INTERVAL,
     DEFAULT_USERNAME,
     DOMAIN,
-    GATEWAY,
-    GATEWAY_PLATFORMS,
-    PW_TYPE,
+    LOGGER,
+    PLATFORMS_GATEWAY,
     SENSOR_PLATFORMS,
-    SERVICE_DELETE,
-    UNDO_UPDATE_LISTENER,
 )
-from .coordinator import PWDataUpdateCoordinator
-from .models import PlugwiseEntityDescription
-
-CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
-
-_LOGGER = logging.getLogger(__name__)
+from .coordinator import PlugwiseDataUpdateCoordinator
 
 
 async def async_setup_entry_gw(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Plugwise Smiles from a config entry."""
+    await async_migrate_entries(hass, entry.entry_id, async_migrate_entity_entry)
+
     websession = async_get_clientsession(hass, verify_ssl=False)
-
-    # When migrating from Core to beta, add the username to ConfigEntry
-    entry_updates = {}
-    if CONF_USERNAME not in entry.data:
-        data = {**entry.data}
-        data.update({CONF_USERNAME: DEFAULT_USERNAME})
-        entry_updates["data"] = data
-
-    if entry_updates:
-        hass.config_entries.async_update_entry(entry, **entry_updates)
-
     api = Smile(
         host=entry.data[CONF_HOST],
-        username=entry.data[CONF_USERNAME],
+        username=entry.data.get(CONF_USERNAME, DEFAULT_USERNAME),
         password=entry.data[CONF_PASSWORD],
         port=entry.data.get(CONF_PORT, DEFAULT_PORT),
         timeout=30,
@@ -72,69 +43,71 @@ async def async_setup_entry_gw(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     try:
         connected = await api.connect()
-        if not connected:
-            _LOGGER.error("Unable to connect to the Smile/Stretch")
-            raise ConfigEntryNotReady
     except InvalidAuthentication:
-        _LOGGER.error("Invalid username or Smile ID")
+        LOGGER.error("Invalid username or Smile ID")
         return False
     except PlugwiseException as err:
-        _LOGGER.error("Error while communicating to the Smile/Stretch")
-        raise ConfigEntryNotReady from err
+        raise ConfigEntryNotReady(
+            f"Error while communicating to device {api.smile_name}"
+        ) from err
     except asyncio.TimeoutError as err:
-        _LOGGER.error("Timeout while connecting to the Smile/Stretch")
-        raise ConfigEntryNotReady from err
+        raise ConfigEntryNotReady(
+            f"Timeout while connecting to Smile {api.smile_name}"
+        ) from err
 
-    # Migrate to a valid unique_id when needed
-    if entry.unique_id is None:
-        if api.smile_version[0] != "1.8.0":
-            hass.config_entries.async_update_entry(entry, unique_id=api.smile_hostname)
+    if not connected:
+        raise ConfigEntryNotReady("Unable to connect to Smile")
+    api.get_all_devices()
+
+    if entry.unique_id is None and api.smile_version[0] != "1.8.0":
+        hass.config_entries.async_update_entry(entry, unique_id=api.smile_hostname)
 
     update_interval = timedelta(
         seconds=entry.options.get(
             CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL[api.smile_type]
         )
     )
-    _LOGGER.debug("DUC update iterval: %s", update_interval)
-    coordinator = PWDataUpdateCoordinator(hass, api, update_interval)
+    LOGGER.debug("DUC update iterval: %s", update_interval)
 
-    api.get_all_devices()
+    coordinator = PlugwiseDataUpdateCoordinator(hass, api)
     await coordinator.async_config_entry_first_refresh()
 
     undo_listener = entry.add_update_listener(_update_listener)
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
-        API: api,
         COORDINATOR: coordinator,
         PW_TYPE: GATEWAY,
         UNDO_UPDATE_LISTENER: undo_listener,
     }
 
-    _LOGGER.debug("Gateway is %s", coordinator.data[0]["gateway_id"])
-    _LOGGER.debug("Gateway software version is %s", api.smile_version[0])
-    _LOGGER.debug("Appliances are %s", coordinator.data[1])
-    s_m_thermostat = coordinator.data[0]["single_master_thermostat"]
-    _LOGGER.debug("Single master thermostat = %s", s_m_thermostat)
+    device_registry = dr.async_get(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        identifiers={(DOMAIN, str(api.gateway_id))},
+        manufacturer="Plugwise",
+        name=entry.title,
+        model=f"Smile {api.smile_name}",
+        sw_version=api.smile_version[0],
+    )
 
-    platforms = GATEWAY_PLATFORMS
-    if s_m_thermostat is None:
+    platforms = PLATFORMS_GATEWAY
+    if coordinator.data.gateway["single_master_thermostat"] is None:
         platforms = SENSOR_PLATFORMS
 
     async def delete_notification(self):
         """Service: delete the Plugwise Notification."""
-        _LOGGER.debug("Service delete PW Notification called for %s", api.smile_name)
+        LOGGER.debug("Service delete PW Notification called for %s", api.smile_name)
         try:
             deleted = await api.delete_notification()
-            _LOGGER.debug("PW Notification deleted: %s", deleted)
+            LOGGER.debug("PW Notification deleted: %s", deleted)
         except PlugwiseException:
-            _LOGGER.debug(
+            LOGGER.debug(
                 "Failed to delete the Plugwise Notification for %s", api.smile_name
             )
 
+    hass.config_entries.async_setup_platforms(entry, platforms)
+
     for component in platforms:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
         if component == Platform.CLIMATE:
             hass.services.async_register(
                 DOMAIN, SERVICE_DELETE, delete_notification, schema=vol.Schema({})
@@ -143,55 +116,28 @@ async def async_setup_entry_gw(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def async_unload_entry_gw(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(
-        entry, GATEWAY_PLATFORMS
-    )
-    if unload_ok:
-        hass.data[DOMAIN][entry.entry_id][UNDO_UPDATE_LISTENER]()
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
-
-
 async def _update_listener(hass: HomeAssistant, entry: ConfigEntry):
     """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-class SmileGateway(CoordinatorEntity):
-    """Represent Smile Gateway."""
+async def async_unload_entry_gw(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS_GATEWAY
+    ):
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
 
-    def __init__(
-        self,
-        coordinator: PWDataUpdateCoordinator,
-        description: PlugwiseEntityDescription,
-        dev_id: str,
-        model: str,
-        name: str,
-        vendor: str,
-        fw: str,
-    ) -> None:
-        """Initialise the gateway."""
-        super().__init__(coordinator)
 
-        entry = coordinator.config_entry
-        gw_id = coordinator.data[0]["gateway_id"]
-        self._attr_available = super().available
-        self._attr_device_class = description.device_class
-        self._attr_device_info = DeviceInfo(
-            configuration_url=f"http://{entry.data[CONF_HOST]}",
-            identifiers={(DOMAIN, dev_id)},
-            manufacturer=vendor,
-            model=model,
-            name=f"Smile {coordinator.data[0]['smile_name']}",
-            sw_version=fw,
-        )
-        self.entity_description = description
+@callback
+def async_migrate_entity_entry(entry: RegistryEntry) -> dict[str, Any] | None:
+    """Migrate Plugwise entity entries.
 
-        if dev_id != gw_id:
-            self._attr_device_info.update(
-                name=name,
-                via_device=(DOMAIN, gw_id),
-            )
+    - Migrates unique ID from old relay switches to the new unique ID
+    """
+    if entry.domain == SWITCH_DOMAIN and entry.unique_id.endswith("-plug"):
+        return {"new_unique_id": entry.unique_id.replace("-plug", "-relay")}
+
+    # No migration needed
+    return None
