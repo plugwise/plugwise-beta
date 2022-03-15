3ec0c43d4196805671a7768a1e12568d96b71052
Tom Scholten <git@scholten.nu>
2022-03-15 20:52:59 +0100

    MVP

--- custom_components/plugwise/select.py
+++ custom_components/plugwise/select.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from collections.abc import Mapping
 from typing import Any
 
-from homeassistant.components.binary_sensor import SelectEntity
+from homeassistant.components.select import SelectEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -16,10 +16,12 @@ from .const import (
     COORDINATOR,
     DOMAIN,
     LOGGER,
+    MASTER_THERMOSTATS,
     PW_TYPE,
     SCHEDULE_ON,
     SERVICE_SELECT,
     SERVICE_SELECT_SCHEMA,
+    USB,
 )
 from .coordinator import PlugwiseDataUpdateCoordinator
 from .entity import PlugwiseEntity
@@ -61,11 +63,12 @@ class PlugwiseSelectEntity(PlugwiseEntity, SelectEntity):
     ) -> None:
         """Initialise the binary_sensor."""
         super().__init__(coordinator, device_id)
-        self._attr_entity_registry_enabled_default = (
-            description.entity_registry_enabled_default
-        )
+        #self._attr_entity_registry_enabled_default = (
+        #    description.entity_registry_enabled_default
+        #)
         self._attr_unique_id = f"{device_id}-select_schema"
         self._attr_name = (f"{self.device.get('name', '')} Select Schema").lstrip()
+        self._attr_current_option = self.device.get("selected_schema")
 
     @property
     def options(self) -> list[str]:
