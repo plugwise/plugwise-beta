5a634df57730583138370b3531148ae5535efd3f
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-03-20 09:02:57 +0100

    Add 2 more unique_id migrates

--- custom_components/plugwise/gateway.py
+++ custom_components/plugwise/gateway.py
@@ -153,8 +153,14 @@ def async_migrate_entity_entry(entry: RegistryEntry) -> dict[str, Any] | None:
 
     - Migrates unique ID from old relay switches to the new unique ID
     """
+    if entry.domain == BINARY_SENSOR_DOMAIN and "auxiliary" in entry.unique_id:
+        return {"new_unique_id": entry.unique_id.replace("-auxiliary", "-opentherm")}
+
     if entry.domain == SWITCH_DOMAIN and entry.unique_id.endswith("-plug"):
         return {"new_unique_id": entry.unique_id.replace("-plug", "-relay")}
+
+    if entry.domain == SENSOR_DOMAIN and entry.unique_id.endswith("-outdoor_temperature"):
+        return {"new_unique_id": entry.unique_id.replace("-outdoor_temperature", "-outdoor_air_temperature")}
 
     # No migration needed
     return None
