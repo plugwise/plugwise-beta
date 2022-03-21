da0fe7880958733c8695aef886acbfa2c06801b3
Tom Scholten <git@scholten.nu>
2022-03-21 19:54:05 +0100

    Local works, lets action

--- scripts/core-testing.sh
+++ scripts/core-testing.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+set -e
 
 # If you want to test a single file
 # run as "scripts/core_testing.sh test_config_flow.py" or
@@ -17,24 +17,32 @@ which git || ( echo "You should have git installed, exiting"; exit 1)
 # use the 'venv', but instantiate it to ensure it works in the
 # ha-core testing later on
 
+# GITHUB ACTIONS $1
+# - core_prep
+# - pip_prep
+# - testing
+# - quality
+
 
 pyversions=(3.9 dummy) # HA-Core is pinned to 3.9
 my_path=$(git rev-parse --show-toplevel)
 my_venv=${my_path}/venv
 
-# Ensures a python virtualenv is available at the highest available python3 version
-for pv in "${pyversions[@]};"; do
-    if [ "$(which "python$pv")" ]; then
-        # If not (yet) available instantiate python virtualenv
-        if [ ! -d "${my_venv}" ]; then
-            "python${pv}" -m venv "${my_venv}"
-            # Ensure wheel is installed (preventing local issues)
-            # shellcheck disable=SC1091
-            . "${my_venv}/bin/activate"
-        fi
-        break
-    fi
-done
+if [ -z "${GITHUB_ACTIONS}"] ; then 
+	# Ensures a python virtualenv is available at the highest available python3 version
+	for pv in "${pyversions[@]};"; do
+	    if [ "$(which "python$pv")" ]; then
+		# If not (yet) available instantiate python virtualenv
+		if [ ! -d "${my_venv}" ]; then
+		    "python${pv}" -m venv "${my_venv}"
+		    # Ensure wheel is installed (preventing local issues)
+		    # shellcheck disable=SC1091
+		    . "${my_venv}/bin/activate"
+		fi
+		break
+	    fi
+	done
+fi
 
 # shellcheck source=/dev/null
 . "${my_venv}/bin/activate"
@@ -63,118 +71,150 @@ fi
 coredir="${my_path}/ha-core/"
 mkdir -p "${coredir}"
 
-# If only dir exists, but not cloned yet
-if [ ! -f "${coredir}/requirements_test_all.txt" ]; then
+if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "core_prep" ] ; then 
+	# If only dir exists, but not cloned yet
+	if [ ! -f "${coredir}/requirements_test_all.txt" ]; then
+		echo ""
+		echo " ** Cloning HA core **"
+		echo ""
+		git clone https://github.com/home-assistant/core.git "${coredir}"
+		if [ ! -f "${coredir}/requirements_test_all.txt" ]; then
+			echo ""
+			echo "Cloning failed .. make sure ${coredir} exists and is an empty directory"
+			echo ""
+			echo "Stopping"
+			echo ""
+			exit 1
+		fi
+		cd "${coredir}" || exit
+		echo ""
+		echo " ** Resetting to dev **"
+		echo ""
+		git config pull.rebase true
+		git checkout dev
+		echo ""
+		echo " ** Running setup script from HA core **"
+		echo ""
+		if [ -z "${GITHUB_ACTIONS}"] ; then 
+			# shellcheck source=/dev/null
+			. "${my_path}/venv/bin/activate"
+			python3 -m venv venv
+		fi
+		python3 -m pip install --upgrade pip 
+		# Not a typo, core setup script resets back to pip 20.3
+		script/setup || python3 -m pip install --upgrade pip 
+		if [ -z "${GITHUB_ACTIONS}"] ; then 
+			# shellcheck source=/dev/null
+			. venv/bin/activate
+		fi
+		echo ""
+		echo " ** Installing test requirements **"
+		echo ""
+		pip install -r requirements_test.txt
+	else
+		cd "${coredir}" || exit
+		echo ""
+		echo " ** Resetting/rebasing core **"
+		echo ""
+		git config pull.rebase true
+		git reset --hard
+		git pull
+	fi
+	# Add tracker
+	git log -1 | head -1 > ${coredir}/.git/plugwise-tracking
+
 	echo ""
-	echo " ** Cloning HA core **"
+	echo "Cleaning existing plugwise from HA core"
 	echo ""
-	git clone https://github.com/home-assistant/core.git "${coredir}"
-        if [ ! -f "${coredir}/requirements_test_all.txt" ]; then
+	rm -r homeassistant/components/plugwise tests/components/plugwise
+	echo ""
+	echo "Overwriting with plugwise-beta"
+	echo ""
+	cp -r ../custom_components/plugwise ./homeassistant/components/
+	cp -r ../tests/components/plugwise ./tests/components/
+	echo ""
+	echo "Adding select to coveragerc as we don't mock this yet"
+	echo ""
+	sed -i".sedbck" 's#homeassistant/scripts/..py#homeassistant/components/plugwise/select.py#' .coveragerc
+	echo ""
+fi # core_prep
+
+if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "pip_prep" ] ; then 
+	cd "${coredir}" || exit
+	if [ -z "${GITHUB_ACTIONS}"] ; then 
+		echo "Activating venv and installing selected test modules (zeroconf,pyserial, etc)"
 		echo ""
-		echo "Cloning failed .. make sure ${coredir} exists and is an empty directory"
+		# shellcheck source=/dev/null
+		. venv/bin/activate
 		echo ""
-		echo "Stopping"
-		echo ""
-		exit 1
-        fi
+	fi
+	python3 -m pip install -q --upgrade pip
+	mkdir -p ./tmp
+	echo ""
+	echo "Installing pip modules"
+	echo ""
+	echo " - HA requirements (core and test)"
+	pip install -q --disable-pip-version-check -r requirements.txt -r requirements_test.txt
+	grep -hEi "fnvhash|lru-dict|voluptuous|aiohttp_cors|pyroute2|sqlalchemy|zeroconf|pyserial|pytest-socket" requirements_test_all.txt > ./tmp/requirements_test_extra.txt
+	echo " - extra's required for plugwise"
+	pip install -q --disable-pip-version-check -r ./tmp/requirements_test_extra.txt
+	echo " - flake8"
+	pip install -q flake8
+	echo ""
+	module=$(grep require ../custom_components/plugwise/manifest.json | cut -f 4 -d '"')
+	echo "Checking manifest for current python-plugwise to install: ${module}"
+	echo ""
+	pip install -q --disable-pip-version-check "${module}"
+fi # pip_prep
+
+if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "testing" ] ; then 
 	cd "${coredir}" || exit
 	echo ""
-	echo " ** Resetting to dev **"
+	echo "Test commencing ..."
 	echo ""
-	git config pull.rebase true
-	git checkout dev
+	# shellcheck disable=SC2086
+	pytest ${subject} --cov=homeassistant/components/plugwise/ --cov-report term-missing -- "tests/components/plugwise/${basedir}" || exit
+fi # testing
+
+if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "quality" ] ; then 
+	cd "${coredir}" || exit
 	echo ""
-	echo " ** Running setup script from HA core **"
+	echo "... flake8-ing component..."
+	flake8 homeassistant/components/plugwise/*py || exit
+	echo "... flak8-ing tests..."
+	flake8 tests/components/plugwise/*py || exit
+	#echo "... pylint-ing component ..."
+	#pylint homeassistant/components/plugwise/*py tests/components/plugwise/*py|| exit
+	echo "... black-ing ..."
+	black homeassistant/components/plugwise/*py tests/components/plugwise/*py || exit
+fi # quality
+
+# Copying back not neccesary in actions
+# hassfest is another action
+if [ -z "${GITHUB_ACTIONS}" ]; then
+	cd "${coredir}" || exit
 	echo ""
-	# shellcheck source=/dev/null
-        . "${my_path}/venv/bin/activate"
-        python3 -m venv venv
-	python3 -m pip install --upgrade pip 
-        # Not a typo, core setup script resets back to pip 20.3
-	script/setup || python3 -m pip install --upgrade pip 
-	# shellcheck source=/dev/null
-        . venv/bin/activate
+	echo "Copy back modified files ..."
 	echo ""
-	echo " ** Installing test requirements **"
+	cp -r ./homeassistant/components/plugwise ../custom_components/
+	cp -r ./tests/components/plugwise ../tests/components/
+	echo "Removing 'version' from manifest for hassfest-ing, version not allowed in core components"
 	echo ""
-        pip install -r requirements_test.txt
-else
-        cd "${coredir}" || exit
-	echo ""
-	echo " ** Resetting/rebasing core **"
-	echo ""
-        git config pull.rebase true
-        git reset --hard
-        git pull
+	# shellcheck disable=SC2090
+	sed -i".sedbck" '/version.:/d' ./homeassistant/components/plugwise/manifest.json
+	grep -q -E 'require.*http.*test-files.pythonhosted.*#' ./homeassistant/components/plugwise/manifest.json && (
+	  echo "Changing requirement for hassfest pass ...."
+	  # shellcheck disable=SC2090
+	  sed -i".sedbck" 's/http.*test-files.pythonhosted.*#//g' ./homeassistant/components/plugwise/manifest.json
+	)
+	echo "Running hassfest for plugwise"
+	python3 -m script.hassfest --integration-path homeassistant/components/plugwise
+	if [ -z "${GITHUB_ACTIONS}"] ; then 
+		deactivate
+	fi
 fi
 
-echo ""
-echo "Cleaning existing plugwise from HA core"
-echo ""
-rm -r homeassistant/components/plugwise tests/components/plugwise
-echo ""
-echo "Overwriting with plugwise-beta"
-echo ""
-cp -r ../custom_components/plugwise ./homeassistant/components/
-cp -r ../tests/components/plugwise ./tests/components/
-echo ""
-echo "Adding select to coveragerc as we don't mock this yet"
-echo ""
-sed -i".sedbck" 's#homeassistant/scripts/..py#homeassistant/components/plugwise/select.py#' .coveragerc
-echo ""
-echo "Activating venv and installing selected test modules (zeroconf,pyserial, etc)"
-echo ""
-# shellcheck source=/dev/null
-. venv/bin/activate
-echo ""
-python3 -m pip install -q --upgrade pip
-mkdir -p ./tmp
-echo ""
-echo "Installing pip modules"
-echo ""
-echo " - HA requirements (core and test)"
-pip install -q --disable-pip-version-check -r requirements.txt -r requirements_test.txt
-grep -hEi "fnvhash|lru-dict|voluptuous|aiohttp_cors|pyroute2|sqlalchemy|zeroconf|pyserial|pytest-socket" requirements_test_all.txt > ./tmp/requirements_test_extra.txt
-echo " - extra's required for plugwise"
-pip install -q --disable-pip-version-check -r ./tmp/requirements_test_extra.txt
-echo " - flake8"
-pip install -q flake8
-echo ""
-module=$(grep require ../custom_components/plugwise/manifest.json | cut -f 4 -d '"')
-echo "Checking manifest for current python-plugwise to install: ${module}"
-echo ""
-pip install -q --disable-pip-version-check "${module}"
-echo ""
-echo "Test commencing ..."
-echo ""
-# shellcheck disable=SC2086
-pytest ${subject} --cov=homeassistant/components/plugwise/ --cov-report term-missing -- "tests/components/plugwise/${basedir}" || exit
-echo ""
-echo "... flake8-ing component..."
-flake8 homeassistant/components/plugwise/*py || exit
-echo "... flak8-ing tests..."
-flake8 tests/components/plugwise/*py || exit
-#echo "... pylint-ing component ..."
-#pylint homeassistant/components/plugwise/*py tests/components/plugwise/*py|| exit
-echo "... black-ing ..."
-black homeassistant/components/plugwise/*py tests/components/plugwise/*py || exit
-echo ""
-echo "Copy back modified files ..."
-echo ""
-cp -r ./homeassistant/components/plugwise ../custom_components/
-cp -r ./tests/components/plugwise ../tests/components/
-echo "Removing 'version' from manifest for hassfest-ing, version not allowed in core components"
-echo ""
-# shellcheck disable=SC2090
-sed -i".sedbck" '/version.:/d' ./homeassistant/components/plugwise/manifest.json
-grep -q -E 'require.*http.*test-files.pythonhosted.*#' ./homeassistant/components/plugwise/manifest.json && (
-  echo "Changing requirement for hassfest pass ...."
-  # shellcheck disable=SC2090
-  sed -i".sedbck" 's/http.*test-files.pythonhosted.*#//g' ./homeassistant/components/plugwise/manifest.json
-)
-echo "Running hassfest for plugwise"
-python3 -m script.hassfest --integration-path homeassistant/components/plugwise
-deactivate
+	#        # disable for further figuring out, apparently HA doesn't pylint against test
+	#        #pylint tests/components/plugwise/*py
 
-#        # disable for further figuring out, apparently HA doesn't pylint against test
-#        #pylint tests/components/plugwise/*py
+
