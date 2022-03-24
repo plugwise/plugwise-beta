b6e2f72cdf3170a0b898a061286d3b3a46325042
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-03-24 20:02:19 +0100

    Async_select_option(): implement @CoMPaTech's suggestion

--- custom_components/plugwise/select.py
+++ custom_components/plugwise/select.py
@@ -111,11 +111,11 @@ class PlugwiseSelectEntity(PlugwiseEntity, SelectEntity):
 
     async def async_select_option(self, option: str) -> None:
         """Change to the selected entity option."""
-        result = await self.async_send_api_call(option, self.entity_description.command)
-        if result:
-            LOGGER.debug(
-                "Set %s to %s was successful", self.entity_description.name, option
-            )
-            await self.coordinator.async_request_refresh()
-        else:
+        if not (
+            await self.async_send_api_call(option, self.entity_description.command)
+        ):
             raise HomeAssistantError(f"Failed to set {self.entity_description.name}")
+        LOGGER.debug(
+            "Set %s to %s was successful", self.entity_description.name, option
+        )
+        await self.coordinator.async_request_refresh()
