a40f751b5030e9a1bae6f8b0b30f67bce507cd3a
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-03-24 18:06:50 +0100

    Update number-testing

--- tests/components/plugwise/test_number.py
+++ tests/components/plugwise/test_number.py
@@ -2,6 +2,9 @@
 
 from unittest.mock import MagicMock
 
+from plugwise.exceptions import PlugwiseException
+import pytest
+
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 
@@ -29,3 +32,28 @@ async def test_anna_number_entities(
     state = hass.states.get("number.opentherm_maximum_boiler_temperature_setpoint")
     assert state
     assert float(state.state) == 60.0
+
+
+async def test_anna_max_boiler_temp_change(
+    hass: HomeAssistant, mock_smile_anna: MagicMock, init_integration: MockConfigEntry
+) -> None:
+    """Test changing of number entities."""
+
+    # enable number entity
+    init_integration.add_to_hass(hass)
+    registry = er.async_get(hass)
+    registry.async_update_entity(
+        "number.opentherm_maximum_boiler_temperature_setpoint", disabled_by=None
+    )
+    await hass.config_entries.async_reload(init_integration.entry_id)
+    await hass.async_block_till_done()
+    
+    await hass.services.async_call(
+        "number",
+        "set_value",
+        {"entity_id": "number.opentherm_maximum_boiler_temperature_setpoint", "value": 65},
+        blocking=True,
+    )
+
+    assert mock_smile_anna.set_max_boiler_temperature.call_count == 1
+    mock_smile_anna.set_max_boiler_temperature.assert_called_with(65)
