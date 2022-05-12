ec4be60e8bb390cdf1686912b7e0c397bc41d179
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-05-12 13:05:13 +0200

    Update assert for the added guarding

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -199,7 +199,7 @@ async def test_anna_climate_entity_climate_changes(
     )
 
     assert mock_smile_anna.set_temperature.call_count == 1
-    assert mock_smile_anna.set_schedule_state.call_count == 1
+    assert mock_smile_anna.set_schedule_state.call_count == 0
     mock_smile_anna.set_schedule_state.assert_called_with(
         "c784ee9fdab44e1395b8dee7d7a497d5", None, "off"
     )
