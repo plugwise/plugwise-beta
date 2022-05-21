c34a71d4e8fdc27d10f8627039d31181a02effbb
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-05-21 13:32:04 +0200

    Don't test delete-notification service

--- custom_components/plugwise/gateway.py
+++ custom_components/plugwise/gateway.py
@@ -121,14 +121,16 @@ async def async_setup_entry_gw(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # pw-beta: HA service - delete_notification
     async def delete_notification(self):
         """Service: delete the Plugwise Notification."""
-        LOGGER.debug("Service delete PW Notification called for %s", api.smile_name)
-        try:
-            deleted = await api.delete_notification()
-            LOGGER.debug("PW Notification deleted: %s", deleted)
-        except PlugwiseException:
+        LOGGER.debug(
+            "Service delete PW Notification called for %s", api.smile_name
+        )  # pragma: no cover
+        try:  # pragma: no cover
+            deleted = await api.delete_notification()  # pragma: no cover
+            LOGGER.debug("PW Notification deleted: %s", deleted)  # pragma: no cover
+        except PlugwiseException:  # pragma: no cover
             LOGGER.debug(
                 "Failed to delete the Plugwise Notification for %s", api.smile_name
-            )
+            )  # pragma: no cover
 
     hass.config_entries.async_setup_platforms(entry, PLATFORMS_GATEWAY)
 
