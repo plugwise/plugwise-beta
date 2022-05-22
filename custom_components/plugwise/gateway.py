21afc92cbccb96b8f68b9db6b017152e42e9e4e5
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-05-22 14:07:39 +0200

    Blacked

--- custom_components/plugwise/gateway.py
+++ custom_components/plugwise/gateway.py
@@ -121,9 +121,7 @@ async def async_setup_entry_gw(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # pw-beta: HA service - delete_notification
     async def delete_notification(self):  # pragma: no cover
         """Service: delete the Plugwise Notification."""
-        LOGGER.debug(
-            "Service delete PW Notification called for %s", api.smile_name
-        )
+        LOGGER.debug("Service delete PW Notification called for %s", api.smile_name)
         try:
             deleted = await api.delete_notification()
             LOGGER.debug("PW Notification deleted: %s", deleted)
