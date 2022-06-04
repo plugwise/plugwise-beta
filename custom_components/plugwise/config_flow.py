773c6073784d01eca39959f1ffa0c4e3d13e905b
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-06-04 09:44:21 +0200

    Blacked

--- custom_components/plugwise/config_flow.py
+++ custom_components/plugwise/config_flow.py
@@ -358,7 +358,9 @@ class PlugwiseOptionsFlowHandler(config_entries.OptionsFlow):
             return self.async_create_entry(title="", data=user_input)
 
         coordinator = self.hass.data[DOMAIN][self.config_entry.entry_id][COORDINATOR]
-        interval: dt.timedelta = DEFAULT_SCAN_INTERVAL[coordinator.api.smile_type]  # pw-beta
+        interval: dt.timedelta = DEFAULT_SCAN_INTERVAL[
+            coordinator.api.smile_type
+        ]  # pw-beta
 
         data = {
             vol.Optional(
