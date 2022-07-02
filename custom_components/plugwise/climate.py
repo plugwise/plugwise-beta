16d073be767c15dc0b682fe4da31433147e704b6
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-07-02 12:44:36 +0200

    Remove import

--- custom_components/plugwise/climate.py
+++ custom_components/plugwise/climate.py
@@ -33,7 +33,6 @@ from .const import (
     CONF_HOMEKIT_EMULATION,  # pw-beta homekit emulation
     COORDINATOR,
     DOMAIN,
-    LOGGER,
     MASTER_THERMOSTATS,
 )
 from .coordinator import PlugwiseDataUpdateCoordinator
