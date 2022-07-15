43c68fec1697e566e36cf97ee79f11eb738b00b7
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-07-15 17:01:23 +0200

    Add missing TEMP_CELCIUS import

--- custom_components/plugwise/number.py
+++ custom_components/plugwise/number.py
@@ -14,6 +14,7 @@ from homeassistant.components.number import (
     NumberMode,
 )
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import TEMP_CELSIUS
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
