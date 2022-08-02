c8ebf096ac26ce500e105cfa7b494a3565e4dfb6
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-08-02 08:10:01 +0200

    Fix assert

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -245,7 +245,7 @@ async def test_anna_climate_entity_climate_changes(
         blocking=True,
     )
 
-    assert mock_smile_anna.set_schedule_state.call_count == 2
+    assert mock_smile_anna.set_schedule_state.call_count == 1
     mock_smile_anna.set_schedule_state.assert_called_with(
         "c784ee9fdab44e1395b8dee7d7a497d5", "standaard", "on"
     )
