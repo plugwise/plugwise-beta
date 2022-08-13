c7ee2e4110c6a1fb8c3f5b53bfa1818a2355f33d
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-08-13 12:20:16 +0200

    Update asserts

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -199,10 +199,9 @@ async def test_anna_climate_entity_attributes(
 
     assert state.attributes["current_temperature"] == 19.3
     assert state.attributes["preset_mode"] == "home"
-    assert state.attributes["supported_features"] == 18
-    assert state.attributes["target_temp_high"] == 24.0
-    assert state.attributes["target_temp_low"] == 21.0
-    assert state.attributes["min_temp"] == 7.0
+    assert state.attributes["supported_features"] == 17
+    assert state.attributes["temperature"] == 20.5
+    assert state.attributes["min_temp"] == 4.0
     assert state.attributes["max_temp"] == 30.0
     assert state.attributes["target_temp_step"] == 0.1
 
@@ -222,9 +221,8 @@ async def test_anna_2_climate_entity_attributes(
         HVAC_MODE_COOL,
         HVAC_MODE_AUTO,
     ]
-    assert state.attributes["target_temp_high"] == 24.0
-    assert state.attributes["target_temp_low"] == 20.5
-    assert state.attributes["supported_features"] == 18
+    assert state.attributes["temperature"] == 24.0
+    assert state.attributes["supported_features"] == 17
 
 
 async def test_anna_3_climate_entity_attributes(
