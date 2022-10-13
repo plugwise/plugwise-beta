128cd201e55c84e5bf32e8336e61d690cad2da7b
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-10-13 20:16:24 +0200

    Correct two temperature asserts

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -199,7 +199,7 @@ async def test_anna_climate_entity_attributes(
     assert state.attributes["preset_mode"] == "home"
     assert state.attributes["supported_features"] == 18
     assert state.attributes["target_temp_high"] == 24.0
-    assert state.attributes["target_temp_low"] == 21.0
+    assert state.attributes["target_temp_low"] == 20.5
     assert state.attributes["min_temp"] == 4.0
     assert state.attributes["max_temp"] == 30.0
     assert state.attributes["target_temp_step"] == 0.1
@@ -221,7 +221,7 @@ async def test_anna_2_climate_entity_attributes(
     ]
     assert state.attributes["supported_features"] == 18
     assert state.attributes["target_temp_high"] == 24.0
-    assert state.attributes["target_temp_low"] == 21.0
+    assert state.attributes["target_temp_low"] == 20.5
 
 
 async def test_anna_3_climate_entity_attributes(
