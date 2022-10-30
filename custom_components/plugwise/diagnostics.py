ccd31af03339dd4682d20ddf57537235838cf7e7
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-10-30 19:54:36 +0100

    Improve diagnostics typing

--- custom_components/plugwise/diagnostics.py
+++ custom_components/plugwise/diagnostics.py
@@ -1,7 +1,7 @@
 """Diagnostics support for Plugwise."""
 from __future__ import annotations
 
-from typing import Any
+from plugwise.constants import GatewayData, DeviceData
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -12,7 +12,7 @@ from .coordinator import PlugwiseDataUpdateCoordinator
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
-) -> dict[str, Any]:
+) -> dict[str, GatewayData | DeviceData]:
     """Return diagnostics for a config entry."""
     coordinator: PlugwiseDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
         COORDINATOR
