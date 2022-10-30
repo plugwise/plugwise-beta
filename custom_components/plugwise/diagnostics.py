ec449fbc3f3ad9a5eef98315de433cf7a13727e2
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-10-30 20:11:29 +0100

    Revert diagnostics changes

--- custom_components/plugwise/diagnostics.py
+++ custom_components/plugwise/diagnostics.py
@@ -1,7 +1,7 @@
 """Diagnostics support for Plugwise."""
 from __future__ import annotations
 
-from plugwise.constants import GatewayData, DeviceData
+from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -12,7 +12,7 @@ from .coordinator import PlugwiseDataUpdateCoordinator
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
-) -> dict[str: GatewayData, str: DeviceData]:
+) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     coordinator: PlugwiseDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
         COORDINATOR
