059efc18264f5a1bd1d0c918b9c2b3f8547e9cb4
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-10-31 08:22:54 +0100

    Move

--- custom_components/plugwise/number.py
+++ custom_components/plugwise/number.py
@@ -112,32 +112,32 @@ class PlugwiseNumberEntity(PlugwiseEntity, NumberEntity):
     def native_step(self) -> float:
         """Return the setpoint step value."""
         return max(
-            self.device[self.entity_description.key][
+            self.device[self.entity_description.key][  # type: ignore[literal-required]
                 self.entity_description.native_step_key
             ],
             1,
-        )  # type: ignore[literal-required]
+        )
 
     @property
     def native_value(self) -> float:
         """Return the present setpoint value."""
-        return self.device[self.entity_description.key][
+        return self.device[self.entity_description.key][  # type: ignore[literal-required]
             self.entity_description.native_value_key
-        ]  # type: ignore[literal-required]
+        ]
 
     @property
     def native_min_value(self) -> float:
         """Return the setpoint min. value."""
-        return self.device[self.entity_description.key][
+        return self.device[self.entity_description.key][  # type: ignore[literal-required]
             self.entity_description.native_min_value_key
-        ]  # type: ignore[literal-required]
+        ]
 
     @property
     def native_max_value(self) -> float:
         """Return the setpoint max. value."""
-        return self.device[self.entity_description.key][
+        return self.device[self.entity_description.key][  # type: ignore[literal-required]
             self.entity_description.native_max_value_key
-        ]  # type: ignore[literal-required]
+        ]
 
     async def async_set_native_value(self, value: float) -> None:
         """Change to the new setpoint value."""
