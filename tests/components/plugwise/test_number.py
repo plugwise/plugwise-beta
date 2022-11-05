c3ed63a51058c15af17232eb87cf81878d13a197
Bouwe <bouwe.s.westerdijk@gmail.com>
2022-11-05 10:57:14 +0100

    Change tests - items are no longer default-disabled

--- tests/components/plugwise/test_number.py
+++ tests/components/plugwise/test_number.py
@@ -9,7 +9,6 @@ from homeassistant.components.number import (
 )
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry
 
@@ -18,20 +17,6 @@ async def test_anna_number_entities(
     hass: HomeAssistant, mock_smile_anna: MagicMock, init_integration: MockConfigEntry
 ) -> None:
     """Test creation of a number."""
-
-    # check if the number entity is disabled by default
-    assert not hass.states.get("number.opentherm_maximum_boiler_temperature_setpoint")
-
-    # enable number entity
-    init_integration.add_to_hass(hass)
-    registry = er.async_get(hass)
-    registry.async_update_entity(
-        "number.opentherm_maximum_boiler_temperature_setpoint", disabled_by=None
-    )
-    await hass.config_entries.async_reload(init_integration.entry_id)
-    await hass.async_block_till_done()
-
-    # Check the number value
     state = hass.states.get("number.opentherm_maximum_boiler_temperature_setpoint")
     assert state
     assert float(state.state) == 60.0
@@ -41,16 +26,6 @@ async def test_anna_max_boiler_temp_change(
     hass: HomeAssistant, mock_smile_anna: MagicMock, init_integration: MockConfigEntry
 ) -> None:
     """Test changing of number entities."""
-
-    # enable number entity
-    init_integration.add_to_hass(hass)
-    registry = er.async_get(hass)
-    registry.async_update_entity(
-        "number.opentherm_maximum_boiler_temperature_setpoint", disabled_by=None
-    )
-    await hass.config_entries.async_reload(init_integration.entry_id)
-    await hass.async_block_till_done()
-
     await hass.services.async_call(
         NUMBER_DOMAIN,
         SERVICE_SET_VALUE,
@@ -71,16 +46,6 @@ async def test_adam_dhw_setpoint_change(
     hass: HomeAssistant, mock_smile_adam_2: MagicMock, init_integration: MockConfigEntry
 ) -> None:
     """Test changing of number entities."""
-
-    # enable number entity
-    init_integration.add_to_hass(hass)
-    registry = er.async_get(hass)
-    registry.async_update_entity(
-        "number.opentherm_domestic_hot_water_setpoint", disabled_by=None
-    )
-    await hass.config_entries.async_reload(init_integration.entry_id)
-    await hass.async_block_till_done()
-
     await hass.services.async_call(
         NUMBER_DOMAIN,
         SERVICE_SET_VALUE,
