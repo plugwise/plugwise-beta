274035456a134b7e59e2133ad391a028339b8540
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-01-09 18:07:06 +0100

    Disable voltage sensors
    Follow Core PR #85451

--- custom_components/plugwise/models.py
+++ custom_components/plugwise/models.py
@@ -456,6 +456,7 @@ PW_SENSOR_TYPES: tuple[PlugwiseSensorEntityDescription, ...] = (
         device_class=SensorDeviceClass.VOLTAGE,
         native_unit_of_measurement=UnitOfElectricPotential.VOLT,
         state_class=SensorStateClass.MEASUREMENT,
+        entity_registry_enabled_default=False,
     ),
     PlugwiseSensorEntityDescription(
         key=V_PHASE_TWO,
@@ -464,6 +465,7 @@ PW_SENSOR_TYPES: tuple[PlugwiseSensorEntityDescription, ...] = (
         device_class=SensorDeviceClass.VOLTAGE,
         native_unit_of_measurement=UnitOfElectricPotential.VOLT,
         state_class=SensorStateClass.MEASUREMENT,
+        entity_registry_enabled_default=False,
     ),
     PlugwiseSensorEntityDescription(
         key=V_PHASE_THREE,
@@ -472,6 +474,7 @@ PW_SENSOR_TYPES: tuple[PlugwiseSensorEntityDescription, ...] = (
         device_class=SensorDeviceClass.VOLTAGE,
         native_unit_of_measurement=UnitOfElectricPotential.VOLT,
         state_class=SensorStateClass.MEASUREMENT,
+        entity_registry_enabled_default=False,
     ),
     PlugwiseSensorEntityDescription(
         key=GAS_CONSUMED_CUMULATIVE,
