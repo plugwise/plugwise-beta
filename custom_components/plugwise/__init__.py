570fd43d438291c3b335555f05382ab451ecc111
Tom Scholten <git@scholten.nu>
2023-03-04 12:18:40 +0100

    Improve tagging

--- custom_components/plugwise/__init__.py
+++ custom_components/plugwise/__init__.py
@@ -4,17 +4,17 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 
-from .const import CONF_USB_PATH  # pw-beta
+from .const import CONF_USB_PATH  # pw-beta usb
 from .gateway import async_setup_entry_gw, async_unload_entry_gw
-from .usb import async_setup_entry_usb, async_unload_entry_usb  # pw-beta
+from .usb import async_setup_entry_usb, async_unload_entry_usb  # pw-beta usb
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Plugwise components from a config entry."""
     if entry.data.get(CONF_HOST):
         return await async_setup_entry_gw(hass, entry)
-    if entry.data.get(CONF_USB_PATH):  # pw-beta
-        return await async_setup_entry_usb(hass, entry)  # pw-beta
+    if entry.data.get(CONF_USB_PATH):  # pw-beta usb
+        return await async_setup_entry_usb(hass, entry)  # pw-beta usb
     return False  # pragma: no cover
 
 
@@ -22,6 +22,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload the Plugwise components."""
     if entry.data.get(CONF_HOST):
         return await async_unload_entry_gw(hass, entry)
-    if entry.data.get(CONF_USB_PATH):  # pw-beta
-        return await async_unload_entry_usb(hass, entry)  # pw-beta
+    if entry.data.get(CONF_USB_PATH):  # pw-beta usb
+        return await async_unload_entry_usb(hass, entry)  # pw-beta usb
     return False  # pragma: no cover
