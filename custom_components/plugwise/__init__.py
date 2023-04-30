7c86fb012421ebbed4cb444ae42d17cd2154e032
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-04-30 11:14:23 +0200

    Typing fixes

--- custom_components/plugwise/__init__.py
+++ custom_components/plugwise/__init__.py
@@ -61,18 +61,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     async def delete_notification(
         self,
-    ):  # pragma: no cover  # pw-beta: HA service - delete_notification
+    ) -> None:  # pragma: no cover  # pw-beta: HA service - delete_notification
         """Service: delete the Plugwise Notification."""
         LOGGER.debug(
-            "Service delete PW Notification called for %s", coordinator.api.smile_name
+            "Service delete PW Notification called for %s",
+            self.coordinator.api.smile_name,
         )
         try:
-            deleted = await coordinator.api.delete_notification()
-            LOGGER.debug("PW Notification deleted: %s", deleted)
+            await self.coordinator.api.delete_notification()
+            LOGGER.debug("PW Notification deleted")
         except PlugwiseError:
             LOGGER.debug(
                 "Failed to delete the Plugwise Notification for %s",
-                coordinator.api.smile_name,
+                self.coordinator.api.smile_name,
             )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS_GATEWAY)
@@ -88,12 +89,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def _update_listener(
     hass: HomeAssistant, entry: ConfigEntry
-):  # pragma: no cover  # pw-beta
+) -> None:  # pragma: no cover  # pw-beta
     """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(
         entry, PLATFORMS_GATEWAY
