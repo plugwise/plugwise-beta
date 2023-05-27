047bb22aec62a2ef0424b80a3e9e819d3583f8c0
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-05-27 10:36:39 +0200

    Number: use UnitOfTemperature

--- custom_components/plugwise/number.py
+++ custom_components/plugwise/number.py
@@ -11,7 +11,7 @@ from homeassistant.components.number import (
     NumberMode,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import TEMP_CELSIUS, EntityCategory
+from homeassistant.const import EntityCategory, UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from plugwise import ActuatorData, DeviceData, Smile
@@ -54,7 +54,7 @@ NUMBER_TYPES = (
         native_step_key_fn=lambda data: data["resolution"],
         native_value_fn=lambda data: data["setpoint"],
         actuator_fn=lambda data: data.get("maximum_boiler_temperature"),
-        native_unit_of_measurement=TEMP_CELSIUS,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
     ),
     PlugwiseNumberEntityDescription(
         key="max_dhw_temperature",
@@ -67,7 +67,7 @@ NUMBER_TYPES = (
         native_step_key_fn=lambda data: data["resolution"],
         native_value_fn=lambda data: data["setpoint"],
         actuator_fn=lambda data: data.get("max_dhw_temperature"),
-        native_unit_of_measurement=TEMP_CELSIUS,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
     ),
 )
 
