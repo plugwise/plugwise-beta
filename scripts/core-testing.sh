d82a458f9f74cf17a28c6bfad580ce5563ab8953
Tom Scholten <git@scholten.nu>
2023-06-12 21:10:00 +0200

    No-fail on ruff-fixing

--- scripts/core-testing.sh
+++ scripts/core-testing.sh
@@ -205,9 +205,9 @@ if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "quality" ] ; then
 	cd "${coredir}" || exit
 	echo ""
 	echo "... ruff-ing component..."
-	ruff --fix homeassistant/components/plugwise/*py || exit
+	ruff --fix homeassistant/components/plugwise/*py 
 	echo "... ruff-ing tests..."
-	ruff --fix tests/components/plugwise/*py || exit
+	ruff --fix tests/components/plugwise/*py 
 	echo "... black-ing ..."
 	black homeassistant/components/plugwise/*py tests/components/plugwise/*py || exit
 	echo "... Prepping strict without hassfest ... (for mypy)"
