89cb3456437f38b8016f4329022e05c68ea602e3
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-07-05 10:46:29 +0200

    Remove translation_key for battery

--- custom_components/plugwise/models.py
+++ custom_components/plugwise/models.py
@@ -382,7 +382,6 @@ PW_SENSOR_TYPES: tuple[PlugwiseSensorEntityDescription, ...] = (
     ),
     PlugwiseSensorEntityDescription(
         key="battery",
-        translation_key="battery",
         device_class=SensorDeviceClass.BATTERY,
         entity_category=EntityCategory.DIAGNOSTIC,
         native_unit_of_measurement=PERCENTAGE,
