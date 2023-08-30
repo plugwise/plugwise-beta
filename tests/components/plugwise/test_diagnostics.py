14ff45f606c8cbf3575acbc521e7f46b993990bf
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2023-08-30 15:06:15 +0200

    test_diagnostic.py: line up with Core

--- tests/components/plugwise/test_diagnostics.py
+++ tests/components/plugwise/test_diagnostics.py
@@ -1,20 +1,21 @@
 """Tests for the diagnostics data provided by the Plugwise integration."""
 from unittest.mock import MagicMock
 
-from aiohttp import ClientSession
 from homeassistant.core import HomeAssistant
 
 from tests.common import MockConfigEntry
 from tests.components.diagnostics import get_diagnostics_for_config_entry
+from tests.typing import ClientSessionGenerator
 
 
 async def test_diagnostics(
     hass: HomeAssistant,
-    hass_client: ClientSession,
+    hass_client: ClientSessionGenerator,
     mock_smile_adam: MagicMock,
     init_integration: MockConfigEntry,
 ) -> None:
     """Test diagnostics."""
+
     assert await get_diagnostics_for_config_entry(
         hass, hass_client, init_integration
     ) == {
