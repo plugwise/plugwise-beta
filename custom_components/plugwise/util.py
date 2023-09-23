daf7689c104958a6386bc956057183f057e6e4ba
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-09-23 20:01:52 +0200

    Remove debugging, enable removing

--- custom_components/plugwise/util.py
+++ custom_components/plugwise/util.py
@@ -55,15 +55,12 @@ def _async_cleanup_registry_entries(hass: HomeAssistant, entry_id: str) -> None:
     }
 
     extra_entities = set(entities.keys()).difference(current_unique_ids)
-    LOGGER.debug("HOI entities: %s", set(entities.keys()))
-    LOGGER.debug("HOI current_unique_ids: %s", current_unique_ids)
-    LOGGER.debug("HOI extra_entities: %s", extra_entities)
     if not extra_entities:
         return
 
-    # for entity in extra_entities:
-    #     if entity_registry.async_is_registered(entities[entity]):
-    #         entity_registry.async_remove(entities[entity])
+    for entity in extra_entities:
+        if entity_registry.async_is_registered(entities[entity]):
+            entity_registry.async_remove(entities[entity])
 
     LOGGER.debug(
         ("Clean-up of Plugwise entities: %s entities removed for config entry %s"),
