8efe26e05b66c206141a0c018507c478363586ab
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-09-27 19:07:50 +0200

    test_climate: remove not-needed line

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -312,6 +312,7 @@ async def test_anna_climate_entity_climate_changes(
         {"entity_id": "climate.anna", "hvac_mode": "auto"},
         blocking=True,
     )
+
     assert mock_smile_anna.set_schedule_state.call_count == 1
     mock_smile_anna.set_schedule_state.assert_called_with(
         "c784ee9fdab44e1395b8dee7d7a497d5", "on"
