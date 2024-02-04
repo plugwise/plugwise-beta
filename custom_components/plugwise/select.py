5be9ca88a8ef59e3d8b9084ce224a119192ddd83
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-02-04 19:57:10 +0100

    Implement Core PR #108498, line up sensor-definitions

--- custom_components/plugwise/select.py
+++ custom_components/plugwise/select.py
@@ -38,33 +38,29 @@ SELECT_TYPES = (
     PlugwiseSelectEntityDescription(
         key="select_schedule",
         translation_key="thermostat_schedule",
-        icon="mdi:calendar-clock",
         command=lambda api, loc, opt: api.set_schedule_state(loc, STATE_ON, opt),
         options_key="available_schedules",
     ),
     PlugwiseSelectEntityDescription(
         key="select_regulation_mode",
         translation_key="regulation_mode",
-        icon="mdi:hvac",
         entity_category=EntityCategory.CONFIG,
         command=lambda api, loc, opt: api.set_regulation_mode(opt),
         options_key="regulation_modes",
     ),
     PlugwiseSelectEntityDescription(
-        key="select_gateway_mode",
-        translation_key="gateway_mode",
-        icon="mdi:cog-outline",
-        entity_category=EntityCategory.CONFIG,
-        command=lambda api, loc, opt: api.set_gateway_mode(opt),
-        options_key="gateway_modes",
-    ),
-    PlugwiseSelectEntityDescription(
         key="select_dhw_mode",
         translation_key="dhw_mode",
-        icon="mdi:shower",
         entity_category=EntityCategory.CONFIG,
         command=lambda api, loc, opt: api.set_dhw_mode(opt),
         options_key="dhw_modes",
+    ),
+    PlugwiseSelectEntityDescription(
+        key="select_gateway_mode",
+        translation_key="gateway_mode",
+        entity_category=EntityCategory.CONFIG,
+        command=lambda api, loc, opt: api.set_gateway_mode(opt),
+        options_key="gateway_modes",
     ),
 )
 
