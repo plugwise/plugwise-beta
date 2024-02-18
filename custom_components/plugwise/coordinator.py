0897c9817cf8f07f77f724785eb04796dcc570d9
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-02-18 15:35:13 +0100

    Simplify cleanup_device_registry()

--- custom_components/plugwise/coordinator.py
+++ custom_components/plugwise/coordinator.py
@@ -28,46 +28,25 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, Upda
 from .const import DEFAULT_PORT, DEFAULT_SCAN_INTERVAL, DEFAULT_USERNAME, DOMAIN, LOGGER
 
 
-def remove_stale_devices(
-    data: PlugwiseData,
-    device_registry: dr.DeviceRegistry,
-    via_id: str,
-) -> None:
-    """Process the Plugwise devices present in the device_registry connected to a specific Gateway."""
-    device_list = list(data.devices.keys())
-    for dev_id, device_entry in list(device_registry.devices.items()):
-        if device_entry.via_device_id == via_id:
-            for item in device_entry.identifiers:
-                if item[0] == DOMAIN and item[1] in device_list:
-                    continue
-
-                device_registry.async_remove_device(dev_id)
-                LOGGER.debug(
-                    "Removed device %s %s %s from device_registry",
-                    DOMAIN,
-                    device_entry.model,
-                    dev_id,
-                )
-
-
 def cleanup_device_registry(
     hass: HomeAssistant,
     data: PlugwiseData,
 ) -> None:
     """Remove deleted devices from device-registry."""
     device_registry = dr.async_get(hass)
-    via_id_list: list[list[str]] = []
-    # Collect the required data of the Plugwise Gateway's
-    for device_entry in list(device_registry.devices.values()):
-        if device_entry.manufacturer == "Plugwise" and device_entry.model == "Gateway":
-            for item in device_entry.identifiers:
-                via_id_list.append([item[1], device_entry.id])
+    plugwise_device_list = list(data.devices.keys())
+    for dev_id, device_entry in list(device_registry.devices.items()):
+        for item in device_entry.identifiers:
+            if item[0] == DOMAIN and item[1] in plugwise_device_list:
+                continue
 
-    for via_id in via_id_list:
-        if via_id[0] != data.gateway["gateway_id"]:
-            continue  # pragma: no cover
-
-        remove_stale_devices(data, device_registry, via_id[1])
+            device_registry.async_remove_device(dev_id)
+            LOGGER.debug(
+                "Removed device %s %s %s from device_registry",
+                DOMAIN,
+                device_entry.model,
+                dev_id,
+            )
 
 
 class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[PlugwiseData]):
