9ea0bd7830ff6b4f4da88a23cb429e79b7c35dd3
Bouwe <bouwe.s.westerdijk@gmail.com>
2024-03-27 08:29:17 +0100

    Remove black-formatting, not needed

--- custom_components/plugwise/__init__.py
+++ custom_components/plugwise/__init__.py
@@ -122,12 +122,8 @@ def async_migrate_entity_entry(entry: er.RegistryEntry) -> dict[str, Any] | None
         return {"new_unique_id": entry.unique_id.replace("-slave_boiler_state", "-secondary_boiler_state")}
     if entry.domain == Platform.SWITCH and entry.unique_id.endswith("-plug"):
         return {"new_unique_id": entry.unique_id.replace("-plug", "-relay")}
-    if entry.domain == Platform.SENSOR and entry.unique_id.endswith(
-        "-relative_humidity"
-    ):
-        return {
-            "new_unique_id": entry.unique_id.replace("-relative_humidity", "-humidity")
-        }
+    if entry.domain == Platform.SENSOR and entry.unique_id.endswith("-relative_humidity"):
+        return {"new_unique_id": entry.unique_id.replace("-relative_humidity", "-humidity")}
 
     # No migration needed
     return None
