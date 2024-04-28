6a4576d5a40b35c00f49f287a9fcb76bcf10045c
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-04-28 16:19:55 +0200

    Try 3

--- custom_components/plugwise/coordinator.py
+++ custom_components/plugwise/coordinator.py
@@ -165,7 +165,9 @@ class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[PlugwiseData]):
             LOGGER.debug("HOI removed device(s) found")
             await cleanup_device_registry(self.hass, data, self.config_entry)
 
-        if (self.new_devices := (data.devices.keys() - self.data.devices.keys())):
+        self.new_devices = set()
+        if new_devices := (data.devices.keys() - self.data.devices.keys()):
+            self.new_devices = new_devices
             LOGGER.debug("HOI new device(s) found")
 
         return data
