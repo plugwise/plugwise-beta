a479a97a78504e7b52d4601c8e89a2bebd09edf3
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-04-29 18:04:36 +0200

    Don't redact select_schedule when 'off' or 'None', show schedules as redacted.

--- custom_components/plugwise/diagnostics.py
+++ custom_components/plugwise/diagnostics.py
@@ -9,22 +9,18 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.redact import async_redact_data
 
 from .const import (
-    AVAILABLE_SCHEDULES,
     COORDINATOR,  # pw-beta
     DEVICES,
     DOMAIN,
     GATEWAY,
     MAC_ADDRESS,
-    SELECT_SCHEDULE,
     ZIGBEE_MAC_ADDRESS,
 )
 from .coordinator import PlugwiseDataUpdateCoordinator
 
 KEYS_TO_REDACT = {
     ATTR_NAME,
-    AVAILABLE_SCHEDULES,
     MAC_ADDRESS,
-    SELECT_SCHEDULE,
     ZIGBEE_MAC_ADDRESS
 }
 
@@ -37,5 +33,14 @@ async def async_get_config_entry_diagnostics(
     ]
 
     data = async_redact_data(coordinator.data.devices, KEYS_TO_REDACT)
+
+    for device in data:
+        for key, value in data[device].items():
+            if key == "select_schedule" and value not in ("off", "None"):
+                data[device][key] = "**REDACTED**"
+            if key == "available_schedules":
+                for i in range(len(value)):
+                    value[i] = "**REDACTED**"
+                data[device][key] = value
 
     return {GATEWAY: coordinator.data.gateway, DEVICES: data}
