4ce17be875fa1ebfd2a65b09310e6df69bdde779
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-04-30 15:40:25 +0200

    Show the related redacted select_schedule

--- custom_components/plugwise/diagnostics.py
+++ custom_components/plugwise/diagnostics.py
@@ -9,11 +9,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.redact import async_redact_data
 
 from .const import (
+    AVAILABLE_SCHEDULES,
     COORDINATOR,  # pw-beta
     DEVICES,
     DOMAIN,
     GATEWAY,
     MAC_ADDRESS,
+    NONE,
+    SELECT_SCHEDULE,
     ZIGBEE_MAC_ADDRESS,
 )
 from .coordinator import PlugwiseDataUpdateCoordinator
@@ -23,6 +26,8 @@ KEYS_TO_REDACT = {
     MAC_ADDRESS,
     ZIGBEE_MAC_ADDRESS
 }
+
+OFF = "off"
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
@@ -36,12 +41,20 @@ async def async_get_config_entry_diagnostics(
 
     for device in data:
         for key, value in data[device].items():
-            if key == "select_schedule" and value not in ("off", "None"):
-                data[device][key] = "**REDACTED**"
-            if key == "available_schedules":
+            if key == AVAILABLE_SCHEDULES:
                 for i in range(len(value)):
-                    if value[i] not in ("off", "None"):
+                    if value[i] not in (OFF, NONE):
                         value[i] = f"**REDACTED_{i}**"
+
                 data[device][key] = value
+
+            if key == SELECT_SCHEDULE and value not in (OFF, NONE):
+                j = 0
+                for item in data[device][AVAILABLE_SCHEDULES]:
+                    if item == data[device][key]:
+                        break
+                    j += 1
+
+                data[device][key] = f"**REDACTED_{j}**"
 
     return {GATEWAY: coordinator.data.gateway, DEVICES: data}
