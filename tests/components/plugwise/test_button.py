945ff2970aab034bc8fb427a86714b4f3886b73a
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-05-23 17:27:14 +0200

    Add button-test-case

--- tests/components/plugwise/test_button.py
+++ tests/components/plugwise/test_button.py
@@ -3,8 +3,14 @@
 from unittest.mock import MagicMock
 
 from homeassistant.components.button import ButtonDeviceClass
-from homeassistant.const import ATTR_DEVICE_CLASS, STATE_UNKNOWN
+from homeassistant.const import (
+    ATTR_DEVICE_CLASS,
+    ATTR_ENTITY_ID,
+    STATE_UNKNOWN,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from tests.common import MockConfigEntry
 
@@ -18,17 +24,18 @@ async def test_adam_reboot_button(
     assert state.state == STATE_UNKNOWN
     assert state.attributes.get(ATTR_DEVICE_CLASS) == ButtonDeviceClass.RESTART
 
-    # registry = er.async_get(hass)
-    # entry = registry.async_get("button.adam_restart")
-    # assert entry
-    # assert entry.unique_id == f"{MAC}_reboot"
-    #
-    # await hass.services.async_call(
-    #     BUTTON_DOMAIN,
-    #     "press",
-    #     {ATTR_ENTITY_ID: "button.adam_restart"},
-    #     blocking=True,
-    # )
-    # await hass.async_block_till_done()
-    #
-    # Test if called
+    registry = er.async_get(hass)
+    entry = registry.async_get("button.adam_reboot")
+    assert entry
+    assert entry.unique_id == "fe799307f1624099878210aa0b9f1475-reboot"
+
+    await hass.services.async_call(
+        Platform.BUTTON,
+        "press",
+        {ATTR_ENTITY_ID: "button.adam_reboot"},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+
+    assert mock_smile_adam.reboot_gateway.call_count == 1
+    mock_smile_adam.reboot_gateway.assert_called_with()
