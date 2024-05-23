cab61e44374e8f25017fca22ca85ea1954aba247
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-05-23 20:25:02 +0200

    Revert back not non-redacted diagnostics

--- custom_components/plugwise/diagnostics.py
+++ custom_components/plugwise/diagnostics.py
@@ -4,30 +4,14 @@ from __future__ import annotations
 from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_NAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.redact import async_redact_data
 
 from .const import (
-    AVAILABLE_SCHEDULES,
     COORDINATOR,  # pw-beta
-    DEVICES,
     DOMAIN,
-    GATEWAY,
-    MAC_ADDRESS,
-    NONE,
-    SELECT_SCHEDULE,
-    ZIGBEE_MAC_ADDRESS,
 )
 from .coordinator import PlugwiseDataUpdateCoordinator
 
-KEYS_TO_REDACT = {
-    ATTR_NAME,
-    MAC_ADDRESS,
-    ZIGBEE_MAC_ADDRESS
-}
-
-OFF = "off"
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, entry: ConfigEntry
@@ -36,20 +20,7 @@ async def async_get_config_entry_diagnostics(
     coordinator: PlugwiseDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
         COORDINATOR
     ]
-
-    data = async_redact_data(coordinator.data.devices, KEYS_TO_REDACT)
-
-    for device in data:
-        if (key := SELECT_SCHEDULE) in data[device]:
-            if (value := data[device][key]) not in (OFF, NONE):
-                i = data[device][AVAILABLE_SCHEDULES].index(value)
-                data[device][key] = f"**REDACTED_{i}**"
-
-            value = data[device][AVAILABLE_SCHEDULES]
-            for j, item in enumerate(value):
-                if item not in (OFF, NONE):
-                    value[j] = f"**REDACTED_{j}**"
-
-            data[device][AVAILABLE_SCHEDULES] = value
-
-    return {GATEWAY: coordinator.data.gateway, DEVICES: data}
+    return {
+        "gateway": coordinator.data.gateway,
+        "devices": coordinator.data.devices,
+    }
