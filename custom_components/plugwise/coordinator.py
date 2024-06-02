3554ff8c426381415a89ab8ce03118b1b4f67225
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-06-02 12:37:58 +0200

    Use try-except-else

--- custom_components/plugwise/coordinator.py
+++ custom_components/plugwise/coordinator.py
@@ -142,20 +142,6 @@ class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[PlugwiseData]):
                 await self._connect()
             data = await self.api.async_update()
             LOGGER.debug(f"{self.api.smile_name} data: %s", data)
-            device_reg = dr.async_get(self.hass)
-            device_list = dr.async_entries_for_config_entry(
-                device_reg, self.config_entry.entry_id
-            )
-            await cleanup_device_and_entity_registry(
-                data,
-                device_reg,
-                device_list,
-                self.config_entry
-            )
-            self.new_devices = len(data.devices.keys()) - len(self.device_list) > 0
-            self.device_list = device_list
-            if self._unavailable_logged:
-                self._unavailable_logged = False
         except InvalidAuthentication as err:
             if not self._unavailable_logged:  # pw-beta add to Core
                 self._unavailable_logged = True
@@ -178,5 +164,21 @@ class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[PlugwiseData]):
             if not self._unavailable_logged:  # pw-beta add to Core
                 self._unavailable_logged = True
                 raise UpdateFailed("Data incomplete or missing") from err
+        else:
+            if self._unavailable_logged:
+                self._unavailable_logged = False
 
-        return data
+            device_reg = dr.async_get(self.hass)
+            device_list = dr.async_entries_for_config_entry(
+                device_reg, self.config_entry.entry_id
+            )
+            await cleanup_device_and_entity_registry(
+                data,
+                device_reg,
+                device_list,
+                self.config_entry
+            )
+            self.new_devices = len(data.devices.keys()) - len(self.device_list) > 0
+            self.device_list = device_list
+
+            return data
