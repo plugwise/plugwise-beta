3389dbb2febb86bb5fd6203cae27e358502a04a2
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-06-20 11:44:38 +0200

    Try 2

--- custom_components/plugwise/config_flow.py
+++ custom_components/plugwise/config_flow.py
@@ -284,7 +284,7 @@ class PlugwiseOptionsFlowHandler(OptionsFlowWithConfigEntry):  # pw-beta options
             )
             return self.async_create_entry(title="", data=user_input)
 
-        coordinator = self._config_entry.runtime_data
+        coordinator = self.hass.data[DOMAIN][self.config_entry.entry_id][COORDINATOR]
         interval: dt.timedelta = DEFAULT_SCAN_INTERVAL[
             coordinator.api.smile_type
         ]  # pw-beta options
