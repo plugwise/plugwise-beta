baec4307f5a2b87b0d7a041a5185bc6ad01401e7
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-06-26 14:53:33 +0200

    Ruffed

--- custom_components/plugwise/button.py
+++ custom_components/plugwise/button.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
 from homeassistant.const import ATTR_NAME, EntityCategory
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import PlugwiseConfigEntry
