98305fb10d57f9baf8a3f9561734441f34113e51
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-09-12 20:04:11 +0200

    Use BINARY_SENSORS constant

--- custom_components/plugwise/binary_sensor.py
+++ custom_components/plugwise/binary_sensor.py
@@ -174,7 +174,7 @@ class PlugwiseBinarySensorEntity(PlugwiseEntity, BinarySensorEntity):
                     self.hass, message, "Plugwise Notification:", f"{DOMAIN}.{notify_id}"
                 )
 
-        return self.device["binary_sensors"][self.entity_description.key]
+        return self.device[BINARY_SENSORS][self.entity_description.key]
 
     @property
     def extra_state_attributes(self) -> Mapping[str, Any] | None:
