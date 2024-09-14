0b964fe9bc88dc206e09bae274ff4fb8ef98a0d8
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-09-14 18:22:08 +0200

    Add model_id to device_registry

--- custom_components/plugwise/__init__.py
+++ custom_components/plugwise/__init__.py
@@ -54,6 +54,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: PlugwiseConfigEntry) -> 
         identifiers={(DOMAIN, str(coordinator.api.gateway_id))},
         manufacturer="Plugwise",
         model=coordinator.api.smile_model,
+        model_id=coordinator.api.smile_model_id,
         name=coordinator.api.smile_name,
         sw_version=coordinator.api.smile_version,
     )  # required for adding the entity-less P1 Gateway
