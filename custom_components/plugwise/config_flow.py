239eb45c5fb8e9e8b0292d351cf0096ffab7f116
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-09-26 19:05:34 +0200

    Try

--- custom_components/plugwise/config_flow.py
+++ custom_components/plugwise/config_flow.py
@@ -150,9 +150,10 @@ class PlugwiseConfigFlow(ConfigFlow, domain=DOMAIN):
                     self.hass,
                     {
                         CONF_HOST: discovery_info.host,
+                        CONF_PASSWORD: config_entry.data[CONF_PASSWORD],
                         CONF_PORT: discovery_info.port,
                         CONF_USERNAME: config_entry.data[CONF_USERNAME],
-                        CONF_PASSWORD: config_entry.data[CONF_PASSWORD],
+                        CONF_TIMEOUT: DEFAULT_TIMEOUT,
                     },
                 )
             except Exception:  # noqa: BLE001
