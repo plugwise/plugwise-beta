acf123ffd612349f220f1c38b3d355a2d173e205
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-01-19 18:55:52 +0100

    Conftest: add new smile-properties

--- tests/components/plugwise/conftest.py
+++ tests/components/plugwise/conftest.py
@@ -36,6 +36,13 @@ def chosen_env(request: pytest.FixtureRequest) -> str:
     """
     return request.param
 
+@pytest.fixture
+def cooling_present(request: pytest.FixtureRequest) -> str:
+    """Pass the cooling_present boolean.
+
+    Used with fixtures that require parametrization of the cooling capability.
+    """
+    return request.param
 
 @pytest.fixture
 def gateway_id(request: pytest.FixtureRequest) -> str:
@@ -45,6 +52,21 @@ def gateway_id(request: pytest.FixtureRequest) -> str:
     """
     return request.param
 
+@pytest.fixture
+def heater_id(request: pytest.FixtureRequest) -> str:
+    """Pass the heater_idstring.
+
+    Used with fixtures that require parametrization of the heater_id.
+    """
+    return request.param
+
+@pytest.fixture
+def reboot(request: pytest.FixtureRequest) -> str:
+    """Pass the reboot boolean.
+
+    Used with fixtures that require parametrization of the reboot capability.
+    """
+    return request.param
 
 @pytest.fixture
 def mock_config_entry() -> MockConfigEntry:
@@ -109,9 +131,11 @@ def mock_smile_adam() -> Generator[MagicMock]:
         smile = smile_mock.return_value
 
         smile.async_update.return_value = all_data["devices"]
+        smile.cooling_present = False
         smile.connect.return_value = Version("3.0.15")
         smile.gateway_id = "fe799307f1624099878210aa0b9f1475"
         smile.heater_id = "90986d591dcd426cae3ec3e8111ff730"
+        smile.reboot = true
         smile.smile_hostname = "smile98765"
         smile.smile_model = "Gateway"
         smile.smile_model_id = "smile_open_therm"
@@ -123,7 +147,7 @@ def mock_smile_adam() -> Generator[MagicMock]:
 
 
 @pytest.fixture
-def mock_smile_adam_heat_cool(chosen_env: str) -> Generator[MagicMock]:
+def mock_smile_adam_heat_cool(chosen_env: str, cooling_present: bool) -> Generator[MagicMock]:
     """Create a special base Mock Adam type for testing with different datasets."""
     all_data = _read_json(chosen_env, "all_data")
     with patch(
@@ -133,8 +157,10 @@ def mock_smile_adam_heat_cool(chosen_env: str) -> Generator[MagicMock]:
 
         smile.async_update.return_value = all_data["devices"]
         smile.connect.return_value = Version("3.6.4")
+        smile.cooling_present = cooling_present
         smile.gateway_id = "da224107914542988a88561b4452b0f6"
         smile.heater_id = "056ee145a816487eaa69243c3280f8bf"
+        smile.reboot = True
         smile.smile_version = "3.6.4"
         smile.smile_type = "thermostat"
         smile.smile_hostname = "smile98765"
@@ -157,8 +183,10 @@ def mock_smile_adam_4() -> Generator[MagicMock]:
 
         smile.async_update.return_value = all_data["devices"]
         smile.connect.return_value = Version("3.2.8")
+        smile.cooling_present = False
         smile.gateway_id = "b5c2386c6f6342669e50fe49dd05b188"
         smile.heater_id = "e4684553153b44afbef2200885f379dc"
+        smile.reboot = True
         smile.smile_hostname = "smile98765"
         smile.smile_model = "Gateway"
         smile.smile_model_id = "smile_open_therm"
@@ -170,7 +198,7 @@ def mock_smile_adam_4() -> Generator[MagicMock]:
 
 
 @pytest.fixture
-def mock_smile_anna(chosen_env: str) -> Generator[MagicMock]:
+def mock_smile_anna(chosen_env: str, cooling_present: bool) -> Generator[MagicMock]:
     """Create a Mock Anna type for testing."""
     all_data = _read_json(chosen_env, "all_data")
     with patch(
@@ -180,8 +208,10 @@ def mock_smile_anna(chosen_env: str) -> Generator[MagicMock]:
 
         smile.async_update.return_value = all_data["devices"]
         smile.connect.return_value = Version("4.0.15")
+        smile.cooling_present = cooling_present
         smile.gateway_id = "015ae9ea3f964e668e490fa39da3870b"
         smile.heater_id = "1cbf783bb11e4a7c8a6843dee3a86927"
+        smile.reboot = True
         smile.smile_version = "4.0.15"
         smile.smile_type = "thermostat"
         smile.smile_hostname = "smile98765"
@@ -205,6 +235,7 @@ def mock_smile_p1(chosen_env: str, gateway_id: str) -> Generator[MagicMock]:
         smile.connect.return_value = Version("4.4.2")
         smile.gateway_id = gateway_id
         smile.heater_id = None
+        smile.reboot = True
         smile.smile_hostname = "smile98765"
         smile.smile_model = "Gateway"
         smile.smile_model_id = "smile"
@@ -229,6 +260,7 @@ def mock_smile_legacy_anna() -> Generator[MagicMock]:
         smile.connect.return_value = Version("1.8.22")
         smile.gateway_id = "0000aaaa0000aaaa0000aaaa0000aa00"
         smile.heater_id = "04e4cbfe7f4340f090f85ec3b9e6a950"
+        smile.reboot = False
         smile.smile_hostname = "smile98765"
         smile.smile_model = "Gateway"
         smile.smile_model_id = None
@@ -253,6 +285,7 @@ def mock_stretch() -> Generator[MagicMock]:
         smile.connect.return_value = Version("3.1.11")
         smile.gateway_id = "259882df3c05415b99c2d962534ce820"
         smile.heater_id = None
+        smile.reboot = False
         smile.smile_hostname = "stretch98765"
         smile.smile_model = "Gateway"
         smile.smile_model_id = None
