811b9be1510d1857386e7f20cab6f6d47d1160e7
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-01-19 19:10:21 +0100

    test-climate fixes

--- tests/components/plugwise/test_climate.py
+++ tests/components/plugwise/test_climate.py
@@ -122,14 +122,14 @@ async def test_adam_3_climate_entity_attributes(
     ]
 
     data = mock_smile_adam_heat_cool.async_update.return_value
-    data.devices["da224107914542988a88561b4452b0f6"][
+    data["da224107914542988a88561b4452b0f6"][
         "select_regulation_mode"
     ] = "heating"
-    data.devices["f2bf9048bef64cc5b6d5110154e33c81"]["control_state"] = "heating"
-    data.devices["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
+    data["f2bf9048bef64cc5b6d5110154e33c81"]["control_state"] = "heating"
+    data["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
         "cooling_state"
     ] = False
-    data.devices["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
+    data["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
         "heating_state"
     ] = True
     with patch(HA_PLUGWISE_SMILE_ASYNC_UPDATE, return_value=data):
@@ -148,14 +148,14 @@ async def test_adam_3_climate_entity_attributes(
         ]
 
     data = mock_smile_adam_heat_cool.async_update.return_value
-    data.devices["da224107914542988a88561b4452b0f6"][
+    data["da224107914542988a88561b4452b0f6"][
         "select_regulation_mode"
     ] = "cooling"
-    data.devices["f2bf9048bef64cc5b6d5110154e33c81"]["control_state"] = "cooling"
-    data.devices["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
+    data["f2bf9048bef64cc5b6d5110154e33c81"]["control_state"] = "cooling"
+    data["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
         "cooling_state"
     ] = True
-    data.devices["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
+    data["056ee145a816487eaa69243c3280f8bf"]["binary_sensors"][
         "heating_state"
     ] = False
     with patch(HA_PLUGWISE_SMILE_ASYNC_UPDATE, return_value=data):
@@ -436,7 +436,7 @@ async def test_anna_climate_entity_climate_changes(
     )
 
     data = mock_smile_anna.async_update.return_value
-    data.devices["3cb70739631c4d17a86b8b12e8a5161b"].pop("available_schedules")
+    data["3cb70739631c4d17a86b8b12e8a5161b"].pop("available_schedules")
     with patch(HA_PLUGWISE_SMILE_ASYNC_UPDATE, return_value=data):
         freezer.tick(timedelta(minutes=1))
         async_fire_time_changed(hass)
