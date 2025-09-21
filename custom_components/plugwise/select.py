f8522155a21f02db83618d599841d6bbf7cf79a4
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-09-21 09:27:10 +0200

    Correct walrus

--- custom_components/plugwise/select.py
+++ custom_components/plugwise/select.py
@@ -139,7 +139,7 @@ class PlugwiseSelectEntity(PlugwiseEntity, SelectEntity):
     @property
     def options(self) -> list[str]:
         """Return the available select-options."""
-        if options := self.device.get(self.entity_description.options_key) is not None:
+        if (options := self.device.get(self.entity_description.options_key)) is not None:
             return options
         return []
 
