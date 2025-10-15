e73e638dc613d6fa60981e6fedf7acff9dcd3943
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-10-15 08:45:11 +0200

    Obtain current_devices from device_registry

--- custom_components/plugwise/coordinator.py
+++ custom_components/plugwise/coordinator.py
@@ -25,6 +25,7 @@ from homeassistant.exceptions import ConfigEntryError
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.debounce import Debouncer
+from homeassistant.helpers.device_registry import DeviceEntry
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from packaging.version import Version
 
@@ -73,8 +74,8 @@ class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[dict[str, GwEntityData
             username=self.config_entry.data[CONF_USERNAME],
             websession=async_get_clientsession(hass, verify_ssl=False),
         )
-        self._current_devices: set[str] = set()
-        self.new_devices: set[str] = set()
+        self._current_devices: list[DeviceEntry] = []
+        self.new_devices: list[str] = []
         self.update_interval = update_interval
 
     async def _connect(self) -> None:
@@ -132,34 +133,32 @@ class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[dict[str, GwEntityData
     async def _async_add_remove_devices(self, data: dict[str, GwEntityData]) -> None:
         """Add new Plugwise devices, remove non-existing devices."""
         # Check for new or removed devices
-        self.new_devices = set(data) - self._current_devices
-        removed_devices = self._current_devices - set(data)
-        self._current_devices = set(data)
+        self.device_registry = dr.async_get(self.hass)
+        self._current_devices = dr.async_entries_for_config_entry(
+            self.device_registry, self.config_entry.entry_id
+        )
+        self.new_devices = list(set(data) - set(self._current_devices))
+        removed_devices = list(set(self._current_devices) - set(data))
 
         if removed_devices:
             await self._async_remove_devices(data)
 
     async def _async_remove_devices(self, data: dict[str, GwEntityData]) -> None:
         """Clean registries when removed devices found."""
-        device_reg = dr.async_get(self.hass)
-        device_list = dr.async_entries_for_config_entry(
-            device_reg, self.config_entry.entry_id
-        )
-
         # First find the Plugwise via_device
-        gateway_device = device_reg.async_get_device({(DOMAIN, self.api.gateway_id)})
+        gateway_device = self.device_reg.async_get_device({(DOMAIN, self.api.gateway_id)})
         if gateway_device is not None:
             via_device_id = gateway_device.id
 
         # Then remove the connected orphaned device(s)
-        for device_entry in device_list:
+        for device_entry in self._current_devices:
             for identifier in device_entry.identifiers:
                 if (
                     identifier[0] == DOMAIN
                     and device_entry.via_device_id == via_device_id
                     and identifier[1] not in data
                 ):
-                    device_reg.async_update_device(
+                    self.device_registry.async_update_device(
                         device_entry.id, remove_config_entry_id=self.config_entry.entry_id
                     )
                     LOGGER.debug(
