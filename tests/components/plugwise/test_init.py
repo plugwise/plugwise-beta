978d021449823550d77c539a10a737863290b204
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-11-28 19:29:40 +0100

    Correct test-asserts

--- tests/components/plugwise/test_init.py
+++ tests/components/plugwise/test_init.py
@@ -297,11 +297,11 @@ async def test_update_device(
 
     assert (
         len(er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id))
-        == 56
+        == 53
     )
     assert (
         len(dr.async_entries_for_config_entry(device_registry, mock_config_entry.entry_id))
-        == 11
+        == 10
     )
 
     # Add a 2nd Tom/Floor
@@ -321,11 +321,11 @@ async def test_update_device(
 
         assert (
             len(er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id))
-            == 63
+            == 60
         )
         assert (
             len(dr.async_entries_for_config_entry(device_registry, mock_config_entry.entry_id))
-            == 12
+            == 11
         )
         item_list: list[str] = []
         for device_entry in device_registry.devices.values():
@@ -348,11 +348,11 @@ async def test_update_device(
 
         assert (
             len(er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id))
-            == 56
+            == 53
         )
         assert (
             len(dr.async_entries_for_config_entry(device_registry, mock_config_entry.entry_id))
-            == 11
+            == 10
         )
         item_list: list[str] = []
         for device_entry in device_registry.devices.values():
