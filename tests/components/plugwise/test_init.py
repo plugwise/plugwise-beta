b2c0fb3419aca6f04d6c8093c459e6bf0b81fe87
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-12-16 17:55:49 +0100

    Clean up

--- tests/components/plugwise/test_init.py
+++ tests/components/plugwise/test_init.py
@@ -27,8 +27,6 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
-
-# from homeassistant.helpers.update_coordinator import UpdateFailed
 from homeassistant.setup import async_setup_component
 
 from tests.common import MockConfigEntry, async_fire_time_changed
