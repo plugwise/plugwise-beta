8b5a41302f8fd0a9e4ec65d18ef3c2002482b863
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-02-20 13:02:23 +0100

    Test: replace zeroconf.ZeroconfServiceInfo

--- tests/components/plugwise/test_config_flow.py
+++ tests/components/plugwise/test_config_flow.py
@@ -46,7 +46,7 @@ TEST_USERNAME = "smile"
 TEST_USERNAME2 = "stretch"
 TEST_SMILE_ID = "smile12345"
 
-TEST_DISCOVERY = zeroconf.ZeroconfServiceInfo(
+TEST_DISCOVERY = ZeroconfServiceInfo(
     ip_address=TEST_HOST,
     ip_addresses=[TEST_HOST],
     # The added `-2` is to simulate mDNS collision
@@ -60,7 +60,7 @@ TEST_DISCOVERY = zeroconf.ZeroconfServiceInfo(
     },
     type="mock_type",
 )
-TEST_DISCOVERY2 = zeroconf.ZeroconfServiceInfo(
+TEST_DISCOVERY2 = ZeroconfServiceInfo(
     ip_address=TEST_HOST,
     ip_addresses=[TEST_HOST],
     hostname=f"{TEST_HOSTNAME2}.local.",
