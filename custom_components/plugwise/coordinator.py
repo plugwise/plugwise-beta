c8864bdf8f7bf5fd105fe09188a7d8709e22edd0
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-03-14 19:45:33 +0100

    Clean up

--- custom_components/plugwise/coordinator.py
+++ custom_components/plugwise/coordinator.py
@@ -208,37 +208,6 @@ class PlugwiseDataUpdateCoordinator(DataUpdateCoordinator[dict[str, GwEntityData
                         device_entry.model,
                         identifier[1],
                     )
-
-    async def _remove_devices(self, data: dict[str, GwEntityData]) -> None:
-        """Clean registries when removed devices found."""
-        device_reg = dr.async_get(self.hass)
-        device_list = dr.async_entries_for_config_entry(
-            device_reg, self.config_entry.entry_id
-        )
-
-        # First find the Plugwise via_device
-        gateway_device = device_reg.async_get_device({(DOMAIN, self.api.gateway_id)})
-        if gateway_device is None:
-            return  # pragma: no cover
-
-        via_device_id = gateway_device.id
-        # Then remove the connected orphaned device(s)
-        for device_entry in device_list:
-            for identifier in device_entry.identifiers:
-                if (
-                    identifier[0] == DOMAIN
-                    and device_entry.via_device_id == via_device_id
-                    and identifier[1] not in data
-                ):
-                    device_reg.async_update_device(
-                        device_entry.id, remove_config_entry_id=self.config_entry.entry_id
-                    )
-                    LOGGER.debug(
-                        "Removed %s device/zone %s %s from device_registry",
-                        DOMAIN,
-                        device_entry.model,
-                        identifier[1],
-                    )
                     break
 
     async def _update_device_firmware(self, data: dict[str, GwEntityData]) -> None:
