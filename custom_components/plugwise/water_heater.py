58cceac8d5ac02297190d117508a6516fdf0288c
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-06-07 13:42:50 +0200

    Add water_heater async_set_temperature()

--- custom_components/plugwise/water_heater.py
+++ custom_components/plugwise/water_heater.py
@@ -4,7 +4,13 @@ from homeassistant.components.water_heater import (
     WaterHeaterEntity,
     WaterHeaterEntityFeature,
 )
-from homeassistant.const import ATTR_NAME, STATE_OFF, STATE_ON, UnitOfTemperature
+from homeassistant.const import (
+    ATTR_NAME,
+    ATTR_TEMPERATURE,
+    STATE_OFF,
+    STATE_ON,
+    UnitOfTemperature,
+)
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
@@ -13,12 +19,14 @@ from .const import (
     DEV_CLASS,
     LOGGER,
     LOWER_BOUND,
+    MAX_DHW_TEMP,
     SENSORS,
     TARGET_TEMP,
     UPPER_BOUND,
 )
 from .coordinator import PlugwiseConfigEntry, PlugwiseDataUpdateCoordinator
 from .entity import PlugwiseEntity
+from .util import plugwise_command
 
 MODE_HEAT = "heat"
 MODE_OFF = "off"
@@ -65,13 +73,17 @@ class PlugwiseWaterHeaterEntity(PlugwiseEntity, WaterHeaterEntity):
     ) -> None:
         """Initialise the water_heater."""
         super().__init__(coordinator, device_id)
+        self.device_id = device_id
         self._attr_unique_id = f"{device_id}-water_heater"
 
         self._attr_max_temp = self.device.get("max_dhw_temperature", {}).get(UPPER_BOUND, 75.0)
         self._attr_min_temp = self.device.get("max_dhw_temperature", {}).get(LOWER_BOUND, 40.0)
         self._attr_supported_features = WaterHeaterEntityFeature.OPERATION_MODE
+        self._supports_temperature_control = False
         if self.device.get("max_dhw_temperature"):
             self._attr_supported_features |= WaterHeaterEntityFeature.TARGET_TEMPERATURE
+            self._supports_temperature_control = True
+
 
     @property
     def current_operation(self) -> str | None:
@@ -91,3 +103,15 @@ class PlugwiseWaterHeaterEntity(PlugwiseEntity, WaterHeaterEntity):
     def target_temperature(self) -> float | None:
         """Return the water temperature we try to reach."""
         return self.device.get("max_dhw_temperature", {}).get(TARGET_TEMP)
+
+    @plugwise_command
+    async def async_set_temperature(self, **kwargs: Any) -> None:
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if not self._supports_temperature_control or temperature is None:
+            return
+
+        await self.coordinator.api.set_number(self.device_id, MAX_DHW_TEMP, temperature)
+        LOGGER.debug(
+            "Setting %s to %s was successful", MAX_DHW_TEMP, temperature
+        )
