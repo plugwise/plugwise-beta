3c3ee4832792fcaaa46cf2db62ef8d8616c4ceaa
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-06-07 13:55:06 +0200

    Add test case

--- tests/components/plugwise/test_water_heater.py
+++ tests/components/plugwise/test_water_heater.py
@@ -4,7 +4,8 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from homeassistant.components.water_heater import DOMAIN as WATER_HEATER_DOMAIN
+from homeassistant.components.water_heater import DOMAIN as WATER_HEATER_DOMAIN, SERVICE_SET_TEMPERATURE
+from homeassistant.const import ATTR_ENTITY_ID, ATTR_TEMPERATURE
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
 from syrupy.assertion import SnapshotAssertion
@@ -27,6 +28,22 @@ async def test_adam_water_heater_snapshot(
 ) -> None:
     """Test Adam water_heater snapshot with dhw_state off."""
     await snapshot_platform(hass, entity_registry, snapshot, setup_platform.entry_id)
+
+
+async def test_adam_water_heater_setpoint_change(
+    hass: HomeAssistant, mock_smile_adam_jip: MagicMock, init_integration: MockConfigEntry
+) -> None:
+    """Test Adam water_heater setpoint-change."""
+    await hass.services.async_call(
+        WATER_HEATER_DOMAIN,
+        SERVICE_SET_TEMPERATURE,
+        {ATTR_ENTITY_ID: "water_heater.opentherm", ATTR_TEMPERATURE: 65},
+        blocking=True,
+    )
+    assert mock_smile_adam_jip.set_number.call_count == 1
+    mock_smile_adam_jip.set_number.assert_called_with(
+        "e4684553153b44afbef2200885f379dc", "max_dhw_temperature", 65.0,
+    )
 
 
 @pytest.mark.parametrize("chosen_env", ["anna_v4_dhw"], indirect=True)
