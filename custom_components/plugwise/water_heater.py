71bd82497f27c07b6a3fb9fc0464347e542d6988
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-06-14 11:25:43 +0200

    Fix water_heater typing

--- custom_components/plugwise/water_heater.py
+++ custom_components/plugwise/water_heater.py
@@ -6,12 +6,16 @@ from homeassistant.components.water_heater import (
     WaterHeaterEntity,
     WaterHeaterEntityFeature,
 )
-from homeassistant.const import ATTR_NAME, ATTR_TEMPERATURE, UnitOfTemperature
+from homeassistant.const import (
+    ATTR_NAME,
+    ATTR_TEMPERATURE,
+    STATE_OFF,
+    UnitOfTemperature,
+)
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from .const import (
-    BINARY_SENSORS,
     DHW_SETPOINT,
     LOGGER,
     LOWER_BOUND,
@@ -68,7 +72,6 @@ class PlugwiseWaterHeaterEntity(PlugwiseEntity, WaterHeaterEntity):
 
         self._attr_max_temp = self.device.get("max_dhw_temperature", {}).get(UPPER_BOUND, 75.0)
         self._attr_min_temp = self.device.get("max_dhw_temperature", {}).get(LOWER_BOUND, 40.0)
-        self._attr_operation_list = self.device.get("dhw_modes", {})
         self._attr_supported_features = WaterHeaterEntityFeature.OPERATION_MODE
         self._attr_supported_features |= WaterHeaterEntityFeature.TARGET_TEMPERATURE
 
@@ -82,6 +85,13 @@ class PlugwiseWaterHeaterEntity(PlugwiseEntity, WaterHeaterEntity):
     def current_temperature(self) -> float | None:
         """Return the current water temperature."""
         return self.device.get(SENSORS, {}).get("water_temperature")
+
+    @property
+    def operation_list(self) -> list[str]:
+        """Return the list of available operation modes."""
+        if (op_list := self.device.get("dhw_modes", [])):
+            return op_list
+        return [STATE_OFF]
 
     @property
     def target_temperature(self) -> float | None:
@@ -99,5 +109,5 @@ class PlugwiseWaterHeaterEntity(PlugwiseEntity, WaterHeaterEntity):
     @plugwise_command
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set new target temperature."""
-        temperature = kwargs.get(ATTR_TEMPERATURE)
-        await self.coordinator.api.set_number(self._dev_id, MAX_DHW_TEMP, temperature)
+        if (temperature := kwargs.get(ATTR_TEMPERATURE)) is not None:
+            await self.coordinator.api.set_number(self._dev_id, MAX_DHW_TEMP, float(temperature))
