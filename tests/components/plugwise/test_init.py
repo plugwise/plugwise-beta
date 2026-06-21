101faaab606f488d6ad114471ac808804801b909
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-06-21 19:16:45 +0200

    Fix len asserts in test_update_device()

--- tests/components/plugwise/test_init.py
+++ tests/components/plugwise/test_init.py
@@ -318,7 +318,7 @@ async def test_update_device(
 
     assert (
         len(er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id))
-        == 53
+        == 52
     )
     assert (
         len(dr.async_entries_for_config_entry(device_registry, mock_config_entry.entry_id))
@@ -342,7 +342,7 @@ async def test_update_device(
 
         assert (
             len(er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id))
-            == 60
+            == 59
         )
         assert (
             len(dr.async_entries_for_config_entry(device_registry, mock_config_entry.entry_id))
@@ -369,7 +369,7 @@ async def test_update_device(
 
         assert (
             len(er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id))
-            == 53
+            == 52
         )
         assert (
             len(dr.async_entries_for_config_entry(device_registry, mock_config_entry.entry_id))
