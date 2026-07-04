186cf27182f12db8641e05cb20b2c88d29566772
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-07-04 09:00:17 +0200

    Add testcase setting dhw_mode to off

--- tests/components/plugwise/test_water_heater.py
+++ tests/components/plugwise/test_water_heater.py
@@ -1,9 +1,10 @@
 """Tests for the Plugwise water_heater platform."""
-
-from unittest.mock import MagicMock
+from datetime import timedelta
+from unittest.mock import MagicMock, patch
 
 import pytest
 
+from freezegun.api import FrozenDateTimeFactory
 from homeassistant.components.water_heater import (
     ATTR_OPERATION_MODE,
     DOMAIN as WATER_HEATER_DOMAIN,
@@ -16,7 +17,7 @@ from homeassistant.exceptions import ServiceNotSupported
 from homeassistant.helpers import entity_registry as er
 from syrupy.assertion import SnapshotAssertion
 
-from tests.common import MockConfigEntry, snapshot_platform
+from tests.common import MockConfigEntry, async_fire_time_changed, snapshot_platform
 
 HA_PLUGWISE_SMILE_ASYNC_UPDATE = (
     "homeassistant.components.plugwise.coordinator.Smile.async_update"
@@ -85,3 +86,42 @@ async def test_anna_water_heater_snapshot(
 ) -> None:
     """Test Anna water_heater snapshot with dhw_state on."""
     await snapshot_platform(hass, entity_registry, snapshot, setup_platform.entry_id)
+
+
+@pytest.mark.parametrize("chosen_env", ["anna_loria_cooling_active"], indirect=True)
+@pytest.mark.parametrize("cooling_present", [False], indirect=True)
+async def test_anna_water_heater_mode_change(
+    hass: HomeAssistant,
+    mock_smile_anna: MagicMock,
+    init_integration: MockConfigEntry,
+    freezer: FrozenDateTimeFactory,
+) -> None:
+    """Test Anna water_heater dhw_mode changes."""
+    await hass.services.async_call(
+        WATER_HEATER_DOMAIN,
+        SERVICE_SET_OPERATION_MODE,
+        {ATTR_ENTITY_ID: "water_heater.opentherm_dhw_temperature", ATTR_OPERATION_MODE: "off"},
+        blocking=True,
+    )
+    assert mock_smile_anna.set_dhw_mode.call_count == 1
+    mock_smile_anna.set_dhw_mode.assert_called_with(
+        "dhw_mode", "bfb5ee0a88e14e5f97bfa725a760cc49", 5, "off"
+    )
+
+    data = mock_smile_anna.async_update.return_value
+    data["bfb5ee0a88e14e5f97bfa725a760cc49"]["dhw_mode"] = "off"
+    with patch(HA_PLUGWISE_SMILE_ASYNC_UPDATE, return_value=data):
+        freezer.tick(timedelta(minutes=1))
+        async_fire_time_changed(hass)
+        await hass.async_block_till_done()
+
+        await hass.services.async_call(
+            WATER_HEATER_DOMAIN,
+            SERVICE_SET_OPERATION_MODE,
+            {ATTR_ENTITY_ID: "water_heater.opentherm_dhw_temperature", ATTR_OPERATION_MODE: "boost"},
+            blocking=True,
+        )
+        assert mock_smile_anna.set_dhw_mode.call_count == 2
+        mock_smile_anna.set_dhw_mode.assert_called_with(
+            "dhw_mode", "bfb5ee0a88e14e5f97bfa725a760cc49", 5, "boost"
+        )
